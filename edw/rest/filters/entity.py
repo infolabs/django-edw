46c15d3df3c44e2e7337532e72664676f3147ef8
EagerBeager <yarovoy@excentrics.ru>
2016-08-22 19:26:31 +0300

    code refactoring

--- edw/rest/filters/entity.py
+++ edw/rest/filters/entity.py
@@ -6,10 +6,10 @@ from django_filters.widgets import CSVWidget
 
 import rest_framework_filters as filters
 
-from rest_framework.exceptions import ValidationError
 from rest_framework import serializers
 
 from edw.models.entity import BaseEntity
+from edw.rest.filters.decorators import get_from_underscore_or_data
 
 
 class EntityFilter(filters.FilterSet):
@@ -24,18 +24,24 @@ class EntityFilter(filters.FilterSet):
         fields = ['active']
 
     @cached_property
-    def use_cached_decompress(self):
+    @get_from_underscore_or_data([], lambda value: value.split(","))
+    def term_ids(self, value):
         '''
-        :return: `use_cached_decompress` value from `self.data`, default: True
+        :return: `term_ids` value parse from `self._term_ids` or `self.data['term_ids']`, default: []
         '''
-        try:
-            return serializers.BooleanField().to_internal_value(self.data.get('use_cached_decompress'))
-        except ValidationError:
-            return True
+        return serializers.ListField(child=serializers.IntegerField()).to_internal_value(value)
+
+    @cached_property
+    @get_from_underscore_or_data(True)
+    def use_cached_decompress(self, value):
+        '''
+        :return: `use_cached_decompress` value parse from `self._use_cached_decompress` or
+            `self.data['use_cached_decompress']`, default: True
+        '''
+        return serializers.BooleanField().to_internal_value(value)
 
     def filter_terms(self, name, queryset, value):
-        try:
-            terms_ids = serializers.ListField(child=serializers.IntegerField()).to_internal_value(value)
-        except ValidationError:
+        self._term_ids = value
+        if not self.term_ids:
             return queryset
-        return queryset.semantic_filter(terms_ids, use_cached_decompress=self.use_cached_decompress)
+        return queryset.semantic_filter(self.term_ids, use_cached_decompress=self.use_cached_decompress)
