6aee4d10039a56ea5f4d53f21768b34ba199e785
EagerBeager <yarovoy@excentrics.ru>
2016-08-22 14:17:23 +0300

    добавил cached_decompress

--- edw/rest/filters/entity.py
+++ edw/rest/filters/entity.py
@@ -24,8 +24,19 @@ class EntityFilter(filters.FilterSet):
         fields = ['active']
 
     def filter_terms(self, name, queryset, value):
+        '''
+        Semantic filter, use cached decompress by default
+        :param name:
+        :param queryset:
+        :param value:
+        :return:
+        '''
         try:
             terms_ids = serializers.ListField(child=serializers.IntegerField()).to_internal_value(value)
         except ValidationError:
             return queryset
-        return queryset.semantic_filter(terms_ids)
+        try:
+            use_cached_decompress = serializers.BooleanField().to_internal_value(self.data.get('use_cached_decompress'))
+        except ValidationError:
+            use_cached_decompress = True
+        return queryset.semantic_filter(terms_ids, use_cached_decompress=use_cached_decompress)
