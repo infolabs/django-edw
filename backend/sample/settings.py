2df6981b5781ba23ba30522486a6055115e4f1b3
sergey-romanov <rebus_vs@mail.ru>
2016-08-31 00:50:58 +0300

    Рефакторинг 4

--- backend/sample/settings.py
+++ backend/sample/settings.py
@@ -19,16 +19,25 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'edw_sample_db',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
+EDW_DB_ENGINE = os.environ.get('EDW_DB_ENGINE')
+if EDW_DB_ENGINE is None:
+    EDW_DB_ENGINE="sqlite3"
+
+SAMPLE_DBS = {
+    'mysql': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'edw_sample_db',
         'USER': 'edw',
         'PASSWORD': 'edwpass',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+    },
+    'sqlite3': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(PROJECT_DIR, 'db-{}.sqlite3'.format(PROJECT_NAME)),
     }
+}
+
+DATABASES = {
+    'default': SAMPLE_DBS['mysql'] if EDW_DB_ENGINE=="mysql" else SAMPLE_DBS['sqlite3'],
 }
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
