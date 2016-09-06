8912a537514ceae8bc0c41aada5e72fadfb83912
sergey-romanov <rebus_vs@mail.ru>
2016-09-06 15:08:58 +0300

    daily

--- backend/edw/rest/serializers/auth.py
+++ backend/edw/rest/serializers/auth.py
@@ -2,9 +2,10 @@
 from __future__ import unicode_literals
 from django.conf import settings
 from django.template.loader import select_template
-from rest_framework.serializers import CharField
+from rest_framework.serializers import CharField, ModelSerializer as RestModelSerializer
 from rest_auth import serializers
 from edw import settings as edw_settings
+from django.contrib.auth import get_user_model
 
 
 class PasswordResetSerializer(serializers.PasswordResetSerializer):
@@ -30,3 +31,16 @@ class PasswordResetSerializer(serializers.PasswordResetSerializer):
 class PasswordResetConfirmSerializer(serializers.PasswordResetConfirmSerializer):
     new_password1 = CharField(min_length=6, max_length=128)
     new_password2 = CharField(min_length=6, max_length=128)
+
+
+"""
+class RegistrationSerializer(RestModelSerializer):
+    password1 = CharField(min_length=6, max_length=128)
+    password2 = CharField(min_length=6, max_length=128)
+    class Meta:
+        model = get_user_model()
+        fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2')
+
+    def save(self):
+        self.set_password_form.save()
+"""
