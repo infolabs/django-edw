36b5ed796f0e44a5d1d2d25823c9689907fcb252
sergey-romanov <rebus_vs@mail.ru>
2016-09-23 18:18:50 +0300

    daily

--- backend/edw/admin/notification/__init__.py
+++ backend/edw/admin/notification/__init__.py
@@ -3,12 +3,13 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 from django.contrib.auth import get_user_model
-from django.utils.translation import ugettext_lazy as _
 from django.forms import widgets
+from django.utils.translation import ugettext_lazy as _
 
 from collections import OrderedDict
 
 from edw.models.notification import NotificationAttachment
+from edw.models.entity import EntityModel
 
 
 class NotificationAttachmentAdmin(admin.TabularInline):
@@ -17,53 +18,86 @@ class NotificationAttachmentAdmin(admin.TabularInline):
 
 
 class NotificationAdmin(admin.ModelAdmin):
+    USER_CHOICES = (
+        ('', _("Nobody")),
+        (0, _("Customer")),
+    )
 
     list_display = (
         'name',
-        'transition_model_display_name',
-        'transition_display_name',
-        'recipient_display_name',
+        'transition_model',
+        'transition',
+        'recipient',
         'mail_template',
         'num_attachments'
     )
-
-    search_fields = ('name',)
 
     inlines = (NotificationAttachmentAdmin,)
 
     save_as = True
     save_on_top = True
 
+    search_fields = ('name',)
+
     def get_form(self, request, obj=None, **kwargs):
         kwargs.update(widgets={
-            'transition_target': widgets.Select(choices=self.model.get_transition_choices()),
-            'mail_to': widgets.Select(choices=self.model.get_mailto_choices()),
+            'transition_target': widgets.Select(choices=self.get_transition_choices()),
+            'mail_to': widgets.Select(choices=self.get_mailto_choices()),
         })
         return super(NotificationAdmin, self).get_form(request, obj, **kwargs)
 
-    def transition_model_display_name(self, obj):
-        sender, target = obj.transition_target.split(':')
-        Model = self.model.get_senders()[sender]
-        return Model._meta.verbose_name
-    transition_model_display_name.short_description = _("Entity")
+    @staticmethod
+    def get_senders():
+        result = {}
+        Model = EntityModel.materialized
+        for clazz in [Model] + Model.__subclasses__():
+            result[clazz.__name__.lower()] = clazz
+        return result
 
-    def transition_display_name(self, obj):
+    def get_transition_choices(self):
+        choices = {}
+        for clazz in self.get_senders().values():
+            status_fields = [f for f in clazz._meta.fields if f.name == 'status']
+            if status_fields:
+                for transition in status_fields.pop().get_all_transitions(clazz):
+                    if transition.target:
+                        transition_name = "{} - {}".format(
+                            clazz._meta.verbose_name,
+                            clazz.get_transition_name(transition.target)
+                        )
+                        choices[self.model.get_transition_target(clazz, transition.target)] = transition_name
+        return choices.items()
+
+    def get_mailto_choices(self):
+        choices = list(self.USER_CHOICES)
+        for user in get_user_model().objects.filter(is_staff=True):
+            email = '{0} {1} <{2}>'.format(user.first_name, user.last_name, user.email)
+            choices.append((user.id, email))
+        return choices
+
+    def transition_model(self, obj):
         sender, target = obj.transition_target.split(':')
-        Model = self.model.get_senders()[sender]
+        Model = self.get_senders()[sender]
+        return Model._meta.verbose_name
+    transition_model.short_description = _("Entity")
+
+    def transition(self, obj):
+        sender, target = obj.transition_target.split(':')
+        Model = self.get_senders()[sender]
         return Model.get_transition_name(target)
-    transition_display_name.short_description = _("Event")
+    transition.short_description = _("Event")
 
     def num_attachments(self, obj):
         return obj.notificationattachment_set.count()
     num_attachments.short_description = _("Attachments")
 
-    def recipient_display_name(self, obj):
+    def recipient(self, obj):
         try:
             if obj.mail_to > 0:
                 user = get_user_model().objects.get(id=obj.mail_to, is_staff=True)
                 return '{0} {1} <{2}>'.format(user.first_name, user.last_name, user.email)
             else:
-                return OrderedDict(self.model.get_mailto_choices())[obj.mail_to]
+                return OrderedDict(self.USER_CHOICES)[obj.mail_to]
         except (get_user_model().DoesNotExist, KeyError):
             return _("Nobody")
-    recipient_display_name.short_description = _("Recipient")
+    recipient.short_description = _("Recipient")
--- backend/edw/models/notification.py
+++ backend/edw/models/notification.py
@@ -3,7 +3,6 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.db.models import Q
-from django.contrib.auth import get_user_model
 from django.utils.translation import ugettext_lazy as _
 
 from filer.fields.file import FilerFileField
@@ -11,22 +10,7 @@ from post_office.models import EmailTemplate
 
 from edw.settings import APP_LABEL
 
-"""
-Patch class `post_office.models.Email` which fixes https://github.com/ui/django-post_office/issues/116
-and additionally converts HTML messages into plain text messages.
-"""
-from edw.models.email import Email
-from edw.models.entity import EntityModel
 from edw.models.customer import CustomerModel
-
-
-USER_CHOICES = (
-    ('', _("Nobody")),
-    (0, _("Customer")),
-)
-
-TRANSITION_TARGET_PATTERN = "{}:{}"
-TRANSITION_NAME_PATTERN = "{} - {}"
 
 
 class Notification(models.Model):
@@ -54,42 +38,8 @@ class Notification(models.Model):
         return CustomerModel.objects.get(pk=self.mail_to).email
 
     @staticmethod
-    def get_senders():
-        result = {}
-        Model = EntityModel.materialized
-        for clazz in [Model] + Model.__subclasses__():
-            result[clazz.__name__.lower()] = clazz
-        return result
-
-    @staticmethod
     def get_transition_target(sender, target):
-        return TRANSITION_TARGET_PATTERN.format(sender.__name__.lower(), target)
-
-    @staticmethod
-    def get_transition_choices():
-        choices = {}
-        for clazz in Notification().get_senders().values():
-            status_fields = [f for f in clazz._meta.fields if f.name == 'status']
-            if status_fields:
-                for transition in status_fields.pop().get_all_transitions(clazz):
-                    if transition.target:
-                        transition_name = TRANSITION_NAME_PATTERN.format(
-                            clazz._meta.verbose_name,
-                            clazz.get_transition_name(transition.target)
-                        )
-                        choices[TRANSITION_TARGET_PATTERN.format(
-                            clazz.__name__.lower(),
-                            transition.target)
-                        ] = transition_name
-        return choices.items()
-
-    @staticmethod
-    def get_mailto_choices():
-        choices = list(USER_CHOICES)
-        for user in get_user_model().objects.filter(is_staff=True):
-            email = '{0} {1} <{2}>'.format(user.first_name, user.last_name, user.email)
-            choices.append((user.id, email))
-        return choices
+        return '{}:{}'.format(sender.__name__.lower(), target)
 
 
 class NotificationAttachment(models.Model):
