0d450c9c2aebca9e2ee3d3de72c51ab51d35364b
sergey-romanov <rebus_vs@mail.ru>
2016-09-26 21:32:56 +0300

    daily

--- backend/edw/models/mixins/entity/notification.py
+++ backend/edw/models/mixins/entity/notification.py
@@ -1,13 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.utils.translation import ugettext_lazy as _
+
+from jsonfield.fields import JSONField
+
 from edw.models.mixins import ModelMixin
 
 
 class NotificationMixin(ModelMixin):
-    """
-    Dummy mixin
-    """
+    stored_request = JSONField(default={},
+        help_text=_("Parts of the Request objects on the moment of submit."))
+
     @property
     def customer(self):
-        raise NotImplementedError
+        raise NotImplementedError(
+            '{cls}.customer must be implemented.'.format(
+                cls=self.__class__.__name__
+            )
+        )
