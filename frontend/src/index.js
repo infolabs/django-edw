469907a0919d45ca1e2a6e10f8f4b33efdaf685a
sergey-romanov <rebus_vs@mail.ru>
2016-10-06 19:48:01 +0300

    daily

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,9 @@ import { render } from 'react-dom';
 import configureStore from './store/configureStore';
 import Root from './containers/Root';
 
+// => returns compiled css code from style.less, resolves imports and url(...)s
+var css = require("!style!raw!less!./less/styles.less");
+
 const store = configureStore();
 
 render(
