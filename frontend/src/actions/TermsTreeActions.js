f922dc60e38ff9efba4d6249ee6a019f5ffbc2c4
Alexander Mishurov <alexander.m.mishurov@gmail.com>
2016-10-14 19:27:49 +0300

    Implement specification mode

--- frontend/src/actions/TermsTreeActions.js
+++ frontend/src/actions/TermsTreeActions.js
@@ -1,14 +1,14 @@
 import * as types from '../constants/TermsTree';
 
 
-export function getTermsTree(tagged_ids = []) {
+export function getTermsTree(selected = [], tagged = []) {
   // let url = Urls['edw:data-mart-terms-tree'](1, 'json')
   // console.log(Urls)
   // console.log(Urls['edw:datamart_tree'](1, 'json'))
   // nothing!
   let url = '/edw/api/data-marts/1/terms/tree.json'
-  if (tagged_ids && tagged_ids.length > 0)
-    url += '?selected=' + tagged_ids.join()
+  if (selected && selected.length > 0)
+    url += '?selected=' + selected.join()
   return fetch(url, {
     method: 'get',
     headers: {
@@ -18,7 +18,8 @@ export function getTermsTree(tagged_ids = []) {
   }).then(response => response.json()).then(json => ({
     type: types.GET_TERMS_TREE,
     json: json,
-    selected: tagged_ids,
+    selected: selected,
+    tagged: tagged,
   }));
 }
 
