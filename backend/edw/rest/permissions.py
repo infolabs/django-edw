ba9310982ce0a7e5a304a34ddc947091d7865a81
EagerBeager <yarovoy@excentrics.ru>
2017-01-10 21:58:59 +0300

    daily

--- backend/edw/rest/permissions.py
+++ backend/edw/rest/permissions.py
@@ -1,25 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+
 from rest_framework import permissions
 
 from filer.fields.file import FilerFileField
-
-class IsOwnerOrReadOnly(permissions.BasePermission):
-    """
-    Object-level permission to only allow owners of an object to edit it.
-    Assumes the model instance has an `owner` attribute.
-    """
-
-    def has_object_permission(self, request, view, obj):
-        # Read permissions are allowed to any request,
-        # so we'll always allow GET, HEAD or OPTIONS requests.
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        # Instance must have an attribute named `owner`.
-        return obj.owner == request.user
-
 
 
 class IsFilerFileOwnerOrReadOnly(permissions.BasePermission):
@@ -29,9 +14,6 @@ class IsFilerFileOwnerOrReadOnly(permissions.BasePermission):
     """
 
     def has_object_permission(self, request, view, obj):
-
-        # print "*** TEST ***", obj
-
         # Read permissions are allowed to any request,
         # so we'll always allow GET, HEAD or OPTIONS requests.
         if request.method in permissions.SAFE_METHODS:
@@ -41,9 +23,6 @@ class IsFilerFileOwnerOrReadOnly(permissions.BasePermission):
         for field in obj._meta.fields:
             if isinstance(field, FilerFileField):
                 f = getattr(obj, field.name)
-
-                # print "*** owner ***", f.owner
-
                 return f.owner == request.user
         else:
             return False
