e43aab3724e320e073d320aee9408ffa4cb65dcc
EagerBeager <yarovoy@excentrics.ru>
2017-02-08 21:21:47 +0300

    daily

--- backend/edw/rest/filters/entity.py
+++ backend/edw/rest/filters/entity.py
@@ -20,34 +20,28 @@ from edw.models.data_mart import DataMartModel
 from edw.rest.filters.decorators import get_from_underscore_or_data
 
 
-class EntityFilter(filters.FilterSet):
+class BaseEntityFilter(filters.FilterSet):
     """
-    EntityFilter
+    BaseEntityFilter
     """
-    active = filters.MethodFilter()
     terms = filters.MethodFilter(widget=CSVWidget())
     data_mart_pk = filters.MethodFilter()
-    subj = filters.MethodFilter(widget=CSVWidget())
-    rel = filters.MethodFilter(widget=CSVWidget())
-
-    class Meta:
-        model = BaseEntity
-        fields = []
 
     def __init__(self, data, **kwargs):
         try:
             data['_mutable'] = True
         except AttributeError:
             data = data.copy()
+        self.patch_data(data, **kwargs)
+        super(BaseEntityFilter, self).__init__(data, **kwargs)
+
+    def patch_data(self, data, **kwargs):
         tree = TermModel.cached_decompress([], fix_it=True)
         data.update({
-            '_initial_queryset': kwargs['queryset'],
             '_initial_filter_meta': tree,
             '_terms_filter_meta': tree,
-            '_data_mart': None,
-            '_subj_ids': []
+            '_data_mart': None
         })
-        super(EntityFilter, self).__init__(data, **kwargs)
 
     @cached_property
     @get_from_underscore_or_data('terms', [], lambda value: value.split(","))
@@ -57,35 +51,9 @@ class EntityFilter(filters.FilterSet):
         '''
         return serializers.ListField(child=serializers.IntegerField()).to_internal_value(value)
 
-    @cached_property
-    @get_from_underscore_or_data('active', None)
-    def is_active(self, value):
-        '''
-        :return: `is_active` value parse from `self._active` or
-            `self.data['active']`, default: None
-        '''
-        return serializers.BooleanField().to_internal_value(value)
-
-    def filter_active(self, name, queryset, value):
-        self._is_active = value
-        if self.is_active is None:
-            return queryset
-        if self.is_active:
-            self.data['_initial_queryset'] = self.data['_initial_queryset'].active()
-            queryset = queryset.active()
-        else:
-            self.data['_initial_queryset'] = self.data['_initial_queryset'].unactive()
-            queryset = queryset.unactive()
-        return queryset
-
-    @cached_property
-    @get_from_underscore_or_data('use_cached_decompress', True)
-    def use_cached_decompress(self, value):
-        '''
-        :return: `use_cached_decompress` value parse from `self._use_cached_decompress` or
-            `self.data['use_cached_decompress']`, default: True
-        '''
-        return serializers.BooleanField().to_internal_value(value)
+    def filter_terms(self, name, queryset, value):
+        msg = "Method filter_terms() must be implemented by subclass: `{}`"
+        raise NotImplementedError(msg.format(self.__class__.__name__))
 
     @cached_property
     @get_from_underscore_or_data('data_mart_pk', None)
@@ -109,6 +77,60 @@ class EntityFilter(filters.FilterSet):
     @cached_property
     def data_mart_term_ids(self):
         return self.data_mart.active_terms_ids if self.data_mart else []
+
+    def filter_data_mart_pk(self, name, queryset, value):
+        msg = "Method filter_data_mart_pk() must be implemented by subclass: `{}`"
+        raise NotImplementedError(msg.format(self.__class__.__name__))
+
+    @cached_property
+    @get_from_underscore_or_data('use_cached_decompress', True)
+    def use_cached_decompress(self, value):
+        '''
+        :return: `use_cached_decompress` value parse from `self._use_cached_decompress` or
+            `self.data['use_cached_decompress']`, default: True
+        '''
+        return serializers.BooleanField().to_internal_value(value)
+
+
+class EntityFilter(BaseEntityFilter):
+    """
+    EntityFilter
+    """
+    active = filters.MethodFilter()
+    subj = filters.MethodFilter(widget=CSVWidget())
+    rel = filters.MethodFilter(widget=CSVWidget())
+
+    class Meta:
+        model = BaseEntity
+        fields = []
+
+    def patch_data(self, data, **kwargs):
+        data.update({
+            '_initial_queryset': kwargs['queryset'],
+            '_subj_ids': []
+        })
+        super(EntityFilter, self).patch_data(data, **kwargs)
+
+    @cached_property
+    @get_from_underscore_or_data('active', None)
+    def is_active(self, value):
+        '''
+        :return: `is_active` value parse from `self._active` or
+            `self.data['active']`, default: None
+        '''
+        return serializers.BooleanField().to_internal_value(value)
+
+    def filter_active(self, name, queryset, value):
+        self._is_active = value
+        if self.is_active is None:
+            return queryset
+        if self.is_active:
+            self.data['_initial_queryset'] = self.data['_initial_queryset'].active()
+            queryset = queryset.active()
+        else:
+            self.data['_initial_queryset'] = self.data['_initial_queryset'].unactive()
+            queryset = queryset.unactive()
+        return queryset
 
     @cached_property
     def data_mart_rel_ids(self):
@@ -174,7 +196,7 @@ class EntityFilter(filters.FilterSet):
     def _separate_rel_by_key(rel, key, lst):
         i = rel.find(key)
         if i != -1:
-            lst.append(long(rel[:i] + rel[i + 1:]))
+            lst.append(int(rel[:i] + rel[i + 1:]))
             return True
         else:
             return False
@@ -198,7 +220,7 @@ class EntityFilter(filters.FilterSet):
             if not EntityFilter._separate_rel_by_key(x, 'b', rel_b_ids):
                 if not EntityFilter._separate_rel_by_key(x, 'f', rel_f_ids):
                     if not EntityFilter._separate_rel_by_key(x, 'r', rel_r_ids):
-                        rel_b_ids.append(long(x))
+                        rel_b_ids.append(int(x))
         if rel_b_ids:
             rel_f_ids.extend(rel_b_ids)
             rel_r_ids.extend(rel_b_ids)
