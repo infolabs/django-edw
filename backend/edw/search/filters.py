fa6eb9299160ac84942cb43961f6250df46b947b
EagerBeager <yarovoy@excentrics.ru>
2017-02-21 14:20:39 +0300

    daily

--- backend/edw/search/filters.py
+++ backend/edw/search/filters.py
@@ -12,3 +12,12 @@ class HaystackTermFilter(BaseHaystackFilterBackend):
 
     query_builder_class = FilterQueryBuilder
     default_operator = operator.and_
+
+
+    # def build_filters(self, view, filters=None):
+    #     applicable = super(HaystackTermFilter, self).build_filters(view, filters)
+    #
+    #     print "--------- build_filters  ----------"
+    #
+    #
+    #     return applicable
