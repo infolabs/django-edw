90c4e4eb843832232a15284819876cbab0c92e1b
ARTEM YAROVOY <yarovoy@excentrics.ru>
2017-05-08 17:27:11 +0300

    +

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,17 +3,20 @@ import { render } from 'react-dom';
 import configureStore from 'store/configureStore';
 import Root from 'components/Root';
 
+import Singleton from 'singleton-js-es6';
+
+
 // => returns compiled css code from style.less, resolves imports and url(...)s
 var css = require("!style!raw!less!./less/styles.less");
 
-const dataMarts = document.getElementsByClassName('ex-data-mart');
+const data_marts = document.getElementsByClassName('ex-data-mart');
 
-for (const dataMart of Array.from(dataMarts)) {
+
+for (const data_mart of Array.from(data_marts)) {
   const store = configureStore(),
-        dom_attrs = dataMart.attributes;
+        dom_attrs = data_mart.attributes;
   render(
     <Root store={store} dom_attrs={dom_attrs} />,
     data_mart
   );
 }
-
