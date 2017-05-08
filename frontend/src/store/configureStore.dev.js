cff1b201d9710588e3d0604588cc3055961d895d
ARTEM YAROVOY <yarovoy@excentrics.ru>
2017-05-08 13:11:54 +0300

    fix

--- frontend/src/store/configureStore.dev.js
+++ frontend/src/store/configureStore.dev.js
@@ -1,27 +1,34 @@
 import { createStore, compose, applyMiddleware } from 'redux';
-import promise from 'redux-promise';
+// import promise from 'redux-promise';
+import thunkMiddleware from 'redux-thunk';
 import { persistState } from 'redux-devtools';
 import rootReducer from 'reducers';
 import DevTools from 'components/DevTools'
 
+// import { saveStore } from './globalStore'
+
 const enhancer = compose(
-  applyMiddleware(promise),
-  DevTools.instrument(),
-  persistState(
-    window.location.href.match(
-      /[?&]debug_session=([^&#]+)\b/
+    // applyMiddleware(promise),
+    applyMiddleware(thunkMiddleware),
+    DevTools.instrument(),
+    persistState(
+        window.location.href.match(
+            /[?&]debug_session=([^&#]+)\b/
+        )
     )
-  )
 );
 
 export default function configureStore(initialState) {
-  const store = createStore(rootReducer, initialState, enhancer);
+    const store = createStore(rootReducer, initialState, enhancer);
 
-  if (module.hot) {
-    module.hot.accept('../reducers', () =>
-      store.replaceReducer(require('../reducers').default)
-    );
-  }
+    if (module.hot) {
+        module.hot.accept('../reducers', () =>
+            store.replaceReducer(require('../reducers').default)
+        );
+    }
 
-  return store;
+    // mart_id = dom_attrs.getNamedItem('data-data-mart-pk').value
+    // saveStore(store);
+
+    return store;
 }
--- frontend/src/store/configureStore.prod.js
+++ frontend/src/store/configureStore.prod.js
@@ -1,7 +1,15 @@
 import { createStore, applyMiddleware } from 'redux';
-import promise from 'redux-promise';
+// import promise from 'redux-promise';
+import thunkMiddleware from 'redux-thunk';
 import rootReducer from 'reducers';
 
+// import { saveStore } from './globalStore'
+
 export default function configureStore(initialState) {
-  return createStore(rootReducer, initialState, applyMiddleware(promise));
+    // const store = createStore(rootReducer, initialState, applyMiddleware(promise));
+    const store = createStore(rootReducer, initialState, applyMiddleware(thunkMiddleware));
+
+    // mart_id = dom_attrs.getNamedItem('data-data-mart-pk').value
+    // saveStore(store);
+    return store;
 }
