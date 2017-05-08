7d7fbec70f8b0e13e51db43dca22b973fb81d697
ARTEM YAROVOY <yarovoy@excentrics.ru>
2017-05-08 13:35:11 +0300

    fix

--- frontend/src/store/configureStore.prod.js
+++ frontend/src/store/configureStore.prod.js
@@ -1,15 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
-// import promise from 'redux-promise';
-import thunkMiddleware from 'redux-thunk';
+import promise from 'redux-promise';
 import rootReducer from 'reducers';
 
-// import { saveStore } from './globalStore'
-
 export default function configureStore(initialState) {
-    // const store = createStore(rootReducer, initialState, applyMiddleware(promise));
-    const store = createStore(rootReducer, initialState, applyMiddleware(thunkMiddleware));
-
-    // mart_id = dom_attrs.getNamedItem('data-data-mart-pk').value
-    // saveStore(store);
-    return store;
+  return createStore(rootReducer, initialState, applyMiddleware(promise));
 }
