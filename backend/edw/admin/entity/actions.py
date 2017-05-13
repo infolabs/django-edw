0df12240685953e071ff83088126368b44cca415
ARTEM YAROVOY <yarovoy@excentrics.ru>
2017-05-13 18:08:55 +0300

    +

--- backend/edw/admin/entity/actions.py
+++ backend/edw/admin/entity/actions.py
@@ -13,7 +13,7 @@ from django.contrib.admin.utils import model_ngettext
 
 from celery import chain
 
-from edw.tasks import update_entities_terms, update_entities_relations
+from edw.tasks import update_entities_terms#, update_entities_relations
 
 from edw.admin.entity.forms import EntitiesUpdateTermsAdminForm, EntitiesUpdateRelationAdminForm
 
@@ -53,12 +53,14 @@ def update_terms(modeladmin, request, queryset):
             # Return None to display the change list page again.
             return None
 
+    else:
+        form = EntitiesUpdateTermsAdminForm()
+
     if len(queryset) == 1:
         objects_name = force_unicode(opts.verbose_name)
     else:
         objects_name = force_unicode(opts.verbose_name_plural)
 
-    form = EntitiesUpdateTermsAdminForm()
 
     title = _("Update terms for multiple entities")
     context = {
@@ -78,6 +80,11 @@ def update_terms(modeladmin, request, queryset):
 update_terms.short_description = _("Set or unset terms for selected %(verbose_name_plural)s")
 
 
+#--------------------------------
+from edw.models.entity import EntityModel
+from edw.models.term import TermModel
+from edw.models.related import EntityRelationModel
+
 def update_relations(modeladmin, request, queryset):
     """
     Update relations for multiple entities
@@ -87,42 +94,123 @@ def update_relations(modeladmin, request, queryset):
     opts = modeladmin.model._meta
     app_label = opts.app_label
 
+    #--------------------------------
+
+
+    def update_entities_relations(entities_ids, to_set_relation_term_id, to_set_target_id,
+                              to_unset_relation_term_id, to_unset_target_id):
+
+        print ("*** UPDATE_ENTITIES_RELATIONS ***", entities_ids, to_set_relation_term_id, to_set_target_id,
+                              to_unset_relation_term_id, to_unset_target_id)
+
+        does_not_exist_entities_ids = []
+        does_not_exist_targets_ids = []
+        does_not_exist_relations_terms_ids = []
+
+        to_set_relation_term = None
+        if to_set_relation_term_id is not None:
+            try:
+                to_set_relation_term = TermModel.objects.attribute_is_relation().get(id=to_set_relation_term_id)
+            except TermModel.DoesNotExist:
+                does_not_exist_relations_terms_ids.append(to_set_relation_term_id)
+        if to_set_relation_term is not None:
+            if to_set_target_id is not None:
+                to_set_target = None
+                try:
+                    to_set_target = EntityModel.objects.get(id=to_set_target_id)
+                except EntityModel.DoesNotExist:
+                    does_not_exist_targets_ids.append(to_set_target_id)
+                if to_set_target:
+                    for entity_id in entities_ids:
+                        try:
+                            entity = EntityModel.objects.get(id=entity_id)
+                            EntityRelationModel.objects.get_or_create(from_entity=entity, to_entity=to_set_target,
+                                                                          term=to_set_relation_term)
+                        except EntityModel.DoesNotExist:
+                            does_not_exist_entities_ids.append(entity_id)
+
+        to_unset_relation_term = None
+        if to_unset_relation_term_id is not None:
+            try:
+                to_unset_relation_term = TermModel.objects.attribute_is_relation().get(
+                    id=to_unset_relation_term_id)
+            except TermModel.DoesNotExist:
+                does_not_exist_relations_terms_ids.append(to_unset_relation_term_id)
+        if to_unset_relation_term is not None:
+            if to_unset_target_id is not None:
+                to_unset_target = None
+                try:
+                    to_unset_target = EntityModel.objects.get(id=to_unset_target_id)
+                except EntityModel.DoesNotExist:
+                    does_not_exist_targets_ids.append(to_unset_target_id)
+
+                if to_unset_target:
+                    EntityRelationModel.objects.filter(to_entity=to_unset_target, from_entity__id__in=entities_ids,
+                                                       term=to_unset_relation_term).delete()
+
+        return {
+            'entities_ids': entities_ids,
+            'to_set_relation_term_id': to_set_relation_term_id,
+            'to_set_target_id': to_set_target_id,
+            'to_unset_relation_term_id': to_unset_relation_term_id,
+            'to_unset_target_id': to_unset_target_id,
+            'does_not_exist_entities_ids': does_not_exist_entities_ids,
+            'does_not_exist_targets_ids': does_not_exist_targets_ids,
+            'does_not_exist_relation_term_ids': does_not_exist_relations_terms_ids
+        }
+
+
+    # --------------------------------
+
+
     if request.POST.get('post'):
         form = EntitiesUpdateRelationAdminForm(request.POST)
 
         if form.is_valid():
-            to_set_target = form.cleaned_data['to_set_target']
-            to_unset_target = form.cleaned_data['to_unset_target']
             to_set_term = form.cleaned_data['to_set_term']
+            to_set_target = form.cleaned_data['to_set_target']
             to_unset_term = form.cleaned_data['to_unset_term']
+            to_unset_target = form.cleaned_data['to_unset_target']
 
             n = queryset.count()
-            # if n and (to_set or to_unset):
-            #     i = 0
-            #     tasks = []
-            #     while i < n:
-            #         chunk = queryset[i:i + CHUNK_SIZE]
-            #         for obj in chunk:
-            #             obj_display = force_unicode(obj)
-            #             modeladmin.log_change(request, obj, obj_display)
-            #         tasks.append(update_entities_relations.si([x.id for x in chunk], term.id,
-            #                                                   to_set.id if to_set else None,
-            #                                                   to_unset.id if to_unset else None))
-            #         i += CHUNK_SIZE
-            #     chain(reduce(OR, tasks)).apply_async()
-            #
-            #     modeladmin.message_user(request, _("Successfully proceed %(count)d %(items)s.") % {
-            #         "count": n, "items": model_ngettext(modeladmin.opts, n)
-            #     })
+            if n and ((to_set_term and to_set_target) or (to_unset_term and to_unset_target)):
+                i = 0
+                tasks = []
+                while i < n:
+                    chunk = queryset[i:i + CHUNK_SIZE]
+                    for obj in chunk:
+                        obj_display = force_unicode(obj)
+                        modeladmin.log_change(request, obj, obj_display)
+
+                    update_entities_relations([x.id for x in chunk], to_set_term.id if to_set_term else None,
+                                              to_set_target.id if to_set_target else None,
+                                              to_unset_term.id if to_unset_term else None,
+                                              to_unset_target.id if to_unset_target else None)
+
+                    # tasks.append(update_entities_relations.si([x.id for x in chunk], term.id,
+                    #                                           to_set.id if to_set else None,
+                    #                                           to_unset.id if to_unset else None))
+
+                    i += CHUNK_SIZE
+
+                # chain(reduce(OR, tasks)).apply_async()
+
+                modeladmin.message_user(request, _("Successfully proceed %(count)d %(items)s.") % {
+                    "count": n, "items": model_ngettext(modeladmin.opts, n)
+                })
+
             # Return None to display the change list page again.
             return None
+
+    else:
+        form = EntitiesUpdateRelationAdminForm()
 
     if len(queryset) == 1:
         objects_name = force_unicode(opts.verbose_name)
     else:
         objects_name = force_unicode(opts.verbose_name_plural)
 
-    form = EntitiesUpdateRelationAdminForm()
+
 
     title = _("Update relations for multiple entities")
     context = {
--- backend/edw/tasks.py
+++ backend/edw/tasks.py
@@ -30,50 +30,52 @@ def update_entities_terms(entities_ids, to_set_terms_ids, to_unset_terms_ids):
 
 
 @shared_task
-def update_entities_relations(entities_ids, relation_term_id, to_set_target_id, to_unset_target_id):
-    does_not_exist_entities_ids = []
-    does_not_exist_targets_ids = []
-    does_not_exist_relation_term_ids = []
-
-    relation_term = None
-    try:
-        relation_term = TermModel.objects.attribute_is_relation().get(id=relation_term_id)
-    except TermModel.DoesNotExist:
-        does_not_exist_relation_term_ids.append(relation_term_id)
-
-    if relation_term is not None:
-        to_set_target = None
-        if to_set_target_id is not None:
-            try:
-                to_set_target = EntityModel.objects.get(id=to_set_target_id)
-            except EntityModel.DoesNotExist:
-                does_not_exist_targets_ids.append(to_set_target_id)
-
-            if to_set_target:
-                for entity_id in entities_ids:
-                    try:
-                        entity = EntityModel.objects.get(id=entity_id)
-                        EntityRelationModel.objects.get_or_create(from_entity=entity, to_entity=to_set_target,
-                                                                      term=relation_term)
-                    except EntityModel.DoesNotExist:
-                        does_not_exist_entities_ids.append(entity_id)
-
-        to_unset_target = None
-        if to_unset_target_id is not None:
-            try:
-                to_unset_target = EntityModel.objects.get(id=to_unset_target_id)
-            except EntityModel.DoesNotExist:
-                does_not_exist_targets_ids.append(to_unset_target_id)
-
-            if to_unset_target:
-                EntityRelationModel.objects.filter(to_entity=to_unset_target, from_entity__id__in=entities_ids,
-                                                   term=relation_term).delete()
-    return {
-        'entities_ids': entities_ids,
-        'relation_term_id': relation_term_id,
-        'to_set_target_id': to_set_target_id,
-        'to_unset_target_id': to_unset_target_id,
-        'does_not_exist_entities_ids': does_not_exist_entities_ids,
-        'does_not_exist_targets_ids': does_not_exist_targets_ids,
-        'does_not_exist_relation_term_ids': does_not_exist_relation_term_ids
-    }
+def update_entities_relations(entities_ids, to_set_relation_term_id, to_set_target_id,
+                              to_unset_relation_term_id, to_unset_target_id):
+    pass
+    # does_not_exist_entities_ids = []
+    # does_not_exist_targets_ids = []
+    # does_not_exist_relation_term_ids = []
+    #
+    # relation_term = None
+    # try:
+    #     relation_term = TermModel.objects.attribute_is_relation().get(id=relation_term_id)
+    # except TermModel.DoesNotExist:
+    #     does_not_exist_relation_term_ids.append(relation_term_id)
+    #
+    # if relation_term is not None:
+    #     to_set_target = None
+    #     if to_set_target_id is not None:
+    #         try:
+    #             to_set_target = EntityModel.objects.get(id=to_set_target_id)
+    #         except EntityModel.DoesNotExist:
+    #             does_not_exist_targets_ids.append(to_set_target_id)
+    #
+    #         if to_set_target:
+    #             for entity_id in entities_ids:
+    #                 try:
+    #                     entity = EntityModel.objects.get(id=entity_id)
+    #                     EntityRelationModel.objects.get_or_create(from_entity=entity, to_entity=to_set_target,
+    #                                                                   term=relation_term)
+    #                 except EntityModel.DoesNotExist:
+    #                     does_not_exist_entities_ids.append(entity_id)
+    #
+    #     to_unset_target = None
+    #     if to_unset_target_id is not None:
+    #         try:
+    #             to_unset_target = EntityModel.objects.get(id=to_unset_target_id)
+    #         except EntityModel.DoesNotExist:
+    #             does_not_exist_targets_ids.append(to_unset_target_id)
+    #
+    #         if to_unset_target:
+    #             EntityRelationModel.objects.filter(to_entity=to_unset_target, from_entity__id__in=entities_ids,
+    #                                                term=relation_term).delete()
+    # return {
+    #     'entities_ids': entities_ids,
+    #     'relation_term_id': relation_term_id,
+    #     'to_set_target_id': to_set_target_id,
+    #     'to_unset_target_id': to_unset_target_id,
+    #     'does_not_exist_entities_ids': does_not_exist_entities_ids,
+    #     'does_not_exist_targets_ids': does_not_exist_targets_ids,
+    #     'does_not_exist_relation_term_ids': does_not_exist_relation_term_ids
+    # }
