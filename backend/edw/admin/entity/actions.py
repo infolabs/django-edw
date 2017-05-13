e741f1154934f35184a9768ea0283bb8fa9ed110
ARTEM YAROVOY <yarovoy@excentrics.ru>
2017-05-13 16:32:13 +0300

    ++++

--- backend/edw/admin/entity/actions.py
+++ backend/edw/admin/entity/actions.py
@@ -77,6 +77,11 @@ def update_terms(modeladmin, request, queryset):
 update_terms.short_description = _("Set or unset terms for selected %(verbose_name_plural)s")
 
 
+# -------------------------
+from edw.models.entity import EntityModel
+from edw.models.term import TermModel
+from edw.models.related import EntityRelationModel
+
 def update_relations(modeladmin, request, queryset):
     """
     Update relations for multiple entities
@@ -86,21 +91,77 @@ def update_relations(modeladmin, request, queryset):
     opts = modeladmin.model._meta
     app_label = opts.app_label
 
+    # ______________________________
+
+
+    def update_entities_relations(entities_ids, relation_term_id, to_set_target_id, to_unset_target_id):
+        does_not_exist_entities_ids = []
+        does_not_exist_targets_ids = []
+        does_not_exist_relation_term_ids = []
+
+        relation_term = None
+
+        try:
+            relation_term = TermModel.objects.attribute_is_relation().get(id=relation_term_id)
+        except TermModel.DoesNotExist:
+            does_not_exist_relation_term_ids.append(relation_term_id)
+
+
+
+
+        if relation_term is not None:
+
+            to_set_target = None
+            if to_set_target_id is not None:
+                try:
+                    to_set_target = EntityModel.objects.get(id=to_set_target_id)
+                except EntityModel.DoesNotExist:
+                    does_not_exist_targets_ids.append(to_set_target_id)
+
+
+
+            to_unset_target = None
+            if to_unset_target_id is not None:
+                try:
+                    to_unset_target = EntityModel.objects.get(id=to_unset_target_id)
+                except EntityModel.DoesNotExist:
+                    does_not_exist_targets_ids.append(to_unset_target_id)
+
+
+
+
+            for entity_id in entities_ids:
+                try:
+                    entity = EntityModel.objects.get(id=entity_id)
+
+                    if to_set_target:
+                        EntityRelationModel.objects.get_or_create(from_entity=entity, to_entity=to_set_target,
+                                                                  term=relation_term)
+
+                except EntityModel.DoesNotExist:
+                    does_not_exist_entities_ids.append(entity_id)
+
+        return {
+            'entities_ids': entities_ids,
+            'relation_term_id': relation_term_id,
+            'to_set_target_id': to_set_target_id,
+            'to_unset_target_id': to_unset_target_id,
+            'does_not_exist_entities_ids': does_not_exist_entities_ids,
+            'does_not_exist_targets_ids': does_not_exist_targets_ids,
+            'does_not_exist_relation_term_ids': does_not_exist_relation_term_ids
+        }
+
+    # ______________________________
+
     if request.POST.get('post'):
         form = EntitiesUpdateRelationAdminForm(request.POST)
-
-        print ("#######POST")
 
         if form.is_valid():
             to_set = form.cleaned_data['to_set_to_entity']
             to_unset = form.cleaned_data['to_unset_to_entity']
             term = form.cleaned_data['term']
 
-
-            print (">>>>>>>>>>>>>>>>>>>>", to_set, to_unset, term)
-
             n = queryset.count()
-            # if n:
             if n and (to_set or to_unset):
                 i = 0
                 tasks = []
@@ -111,8 +172,11 @@ def update_relations(modeladmin, request, queryset):
                         modeladmin.log_change(request, obj, obj_display)
                     # tasks.append(update_entities_terms.si([x.id for x in chunk], to_set, to_unset))
 
-                    print ('+++ UPDATE RELATIONS', [x.id for x in chunk], to_set.id if to_set else None,
-                           to_unset.id if to_unset else None)
+                    print ('+++ UPDATE RELATIONS', update_entities_relations(
+                        [x.id for x in chunk],
+                        term.id,
+                        to_set.id if to_set else None,
+                        to_unset.id if to_unset else None))
 
                     i += CHUNK_SIZE
 
