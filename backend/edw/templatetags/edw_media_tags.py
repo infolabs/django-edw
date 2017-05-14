99000b783ebbb2646c73381837316b98ab9cc73a
Rebus <romanov@excentrics.ru>
2017-05-15 01:54:20 +0300

    fix

--- backend/edw/templatetags/edw_media_tags.py
+++ backend/edw/templatetags/edw_media_tags.py
@@ -3,17 +3,24 @@ from __future__ import unicode_literals
 from __future__ import division
 
 import re
+import os
 
-from django.utils import six
 from django import template
+from django.core.files import File
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.cache import cache
 from django.template import TemplateSyntaxError
+from django.conf import settings
+from django.utils import six
 
 from classytags.core import Options, Tag
 from classytags.arguments import Argument
 
+from easy_thumbnails.files import get_thumbnailer
+
+from filer.models import Image
 
 register = template.Library()
-
 
 RE_SIZE = re.compile(r'(\d+)x(\d+)$')
 
@@ -52,3 +59,45 @@ class GetAlignThumbnailInCenterStyles(Tag):
         return ''
 
 register.tag(GetAlignThumbnailInCenterStyles)
+
+
+class GetDefaultThumbnail(Tag):
+    name = 'get_default_thumbnail'
+    options = Options(
+        Argument('size', required=True, resolve=True),
+        Argument('replace_alpha', required=True, resolve=True),
+        'as',
+        Argument('varname', required=False, resolve=False)
+    )
+
+    def render_tag(self, context, size, replace_alpha, varname):
+        if isinstance(size, six.string_types):
+            m = RE_SIZE.match(size)
+            if m:
+                size = (int(m.group(1)), int(m.group(2)))
+            else:
+                raise TemplateSyntaxError(
+                    "%r is not a valid size." % size)
+        no_photo_name = "no_photo.png"
+        try:
+            no_photo_image = Image.objects.get(original_filename=no_photo_name)
+        except ObjectDoesNotExist:
+            no_photo_file = os.path.join(settings.STATIC_ROOT, "edw/img/no_photo.png")
+            with open(no_photo_file, "rb") as f:
+                file_obj = File(f, name=no_photo_name)
+                (no_photo_image, is_created) = Image.objects.get_or_create(
+                    original_filename=no_photo_name,
+                    defaults={'file': file_obj, 'owner': context.request.user}
+                )
+        thumbnailer = get_thumbnailer(no_photo_image)
+        thumbnail_options = {
+            'crop': True,
+            "size": size,
+            "replace_alpha": replace_alpha,
+            "upscale": True,
+        }
+        thumb = thumbnailer.get_thumbnail(thumbnail_options)
+        context[varname] = thumb
+        return ''
+
+register.tag(GetDefaultThumbnail)
