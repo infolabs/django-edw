3cc33c1f203d30f99b675ff47b059cda837df943
ARTEM YAROVOY <yarovoy@excentrics.ru>
2017-05-20 19:45:20 +0300

    Черновик функционала смены состояний

---
+++ backend/edw/admin/entity/actions/update_states.py
@@ -0,0 +1,93 @@
+#-*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+
+from operator import __or__ as OR
+from functools import reduce
+
+from django.conf import settings
+from django.utils.encoding import force_unicode
+from django.utils.translation import ugettext_lazy as _
+from django.template.response import TemplateResponse
+from django.contrib.admin import helpers
+from django.contrib.admin.utils import model_ngettext
+
+from celery import chain
+
+from edw.tasks import update_entities_states
+
+from edw.admin.entity.forms import EntitiesUpdateStateAdminForm
+
+
+def update_states(modeladmin, request, queryset):
+    """
+    Update relations for multiple entities
+    """
+    CHUNK_SIZE = getattr(settings, 'EDW_UPDATE_STATES_ACTION_CHUNK_SIZE', 100)
+
+    opts = modeladmin.model._meta
+    app_label = opts.app_label
+
+    if request.POST.get('post'):
+        form = EntitiesUpdateStateAdminForm(request.POST)
+
+        if form.is_valid():
+            # to_set_term = form.cleaned_data['to_set_term']
+            # to_set_targets = form.cleaned_data['to_set_targets']
+            # to_unset_term = form.cleaned_data['to_unset_term']
+            # to_unset_targets = form.cleaned_data['to_unset_targets']
+
+            n = queryset.count()
+            if n:
+            # if n and ((to_set_term and to_set_targets) or (to_unset_term and to_unset_targets)):
+                i = 0
+                tasks = []
+                while i < n:
+                    chunk = queryset[i:i + CHUNK_SIZE]
+                    for obj in chunk:
+                        obj_display = force_unicode(obj)
+                        modeladmin.log_change(request, obj, obj_display)
+
+                    update_entities_states([x.id for x in chunk], 'OLOLOLO!!!')
+
+                    # tasks.append(update_entities_relations.si([x.id for x in chunk],
+                    #                           to_set_term.id if to_set_term else None,
+                    #                           [x.id for x in to_set_targets],
+                    #                           to_unset_term.id if to_unset_term else None,
+                    #                           [x.id for x in to_unset_targets]))
+
+                    i += CHUNK_SIZE
+
+                # chain(reduce(OR, tasks)).apply_async()
+
+                modeladmin.message_user(request, _("Successfully proceed %(count)d %(items)s.") % {
+                    "count": n, "items": model_ngettext(modeladmin.opts, n)
+                })
+
+            # Return None to display the change list page again.
+            return None
+
+    else:
+        form = EntitiesUpdateStateAdminForm()
+
+    if len(queryset) == 1:
+        objects_name = force_unicode(opts.verbose_name)
+    else:
+        objects_name = force_unicode(opts.verbose_name_plural)
+
+    title = _("Update states for multiple entities")
+    context = {
+        "title": title,
+        'form': form,
+        "objects_name": objects_name,
+        'queryset': queryset,
+        "opts": opts,
+        "app_label": app_label,
+        'action_checkbox_name': helpers.ACTION_CHECKBOX_NAME,
+        'media': modeladmin.media,
+    }
+    # Display the confirmation page
+    return TemplateResponse(request, "edw/admin/entities/actions/update_states.html",
+                            context, current_app=modeladmin.admin_site.name)
+
+update_states.short_description = _("Modify state for selected %(verbose_name_plural)s")
---
+++ backend/edw/admin/entity/forms/update_states.py
@@ -0,0 +1,60 @@
+#-*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+from django.contrib import admin
+
+# from salmonella.widgets import SalmonellaMultiIdWidget
+
+# from edw.models.term import TermModel
+# from edw.models.entity import EntityModel
+# from edw.models.related import EntityRelationModel
+
+from edw.admin.mptt.fields import FullPathTreeNodeChoiceField
+
+
+#==============================================================================
+# EntitiesUpdateStateAdminForm
+#==============================================================================
+class EntitiesUpdateStateAdminForm(forms.Form):
+
+    test = forms.CharField(max_length=255)
+
+    # to_set_term = FullPathTreeNodeChoiceField(queryset=TermModel.objects.attribute_is_relation(), required=False,
+    #                                    joiner=' / ', label=_('Relation to set'))
+    #
+    # to_set_targets = forms.ModelMultipleChoiceField(queryset=EntityModel.objects.all(), label=_('Targets to set'),
+    #                                           required=False, widget=SalmonellaMultiIdWidget(
+    #         EntityRelationModel._meta.get_field("to_entity").rel, admin.site))
+    #
+    # to_unset_term = FullPathTreeNodeChoiceField(queryset=TermModel.objects.attribute_is_relation(), required=False,
+    #                                           joiner=' / ', label=_('Relation to unset'))
+    #
+    # to_unset_targets = forms.ModelMultipleChoiceField(queryset=EntityModel.objects.all(), label=_('Targets to unset'),
+    #                                             required=False, widget=SalmonellaMultiIdWidget(
+    #         EntityRelationModel._meta.get_field("to_entity").rel, admin.site))
+    #
+    # def clean(self):
+    #     cleaned_data = super(EntitiesUpdateRelationAdminForm, self).clean()
+    #     to_set_term = cleaned_data.get("to_set_term")
+    #     to_set_targets = cleaned_data.get("to_set_targets")
+    #
+    #     to_unset_term = cleaned_data.get("to_unset_term")
+    #     to_unset_targets = cleaned_data.get("to_unset_targets")
+    #
+    #     if not (to_set_term or to_unset_term):
+    #         raise forms.ValidationError(
+    #             _("Select relation for set or unset")
+    #         )
+    #
+    #     if bool(to_set_term) ^ bool(to_set_targets):
+    #         raise forms.ValidationError(
+    #             _("Select relation and target for set")
+    #         )
+    #
+    #     if bool(to_unset_term) ^ bool(to_unset_targets):
+    #         raise forms.ValidationError(
+    #             _("Select relation and target for unset")
+    #         )
+    #     return cleaned_data
