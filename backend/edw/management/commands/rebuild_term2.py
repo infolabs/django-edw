3d5cd00c67aef9487a55439c32491b852ea1893c
Alexander Mishurov <alexander.m.mishurov@gmail.com>
2017-07-26 17:56:00 +0300

    NR-87 Add a new rebuild command

---
+++ backend/edw/management/commands/rebuild_term2.py
@@ -0,0 +1,20 @@
+# ------------------------------------------------------------------------
+# coding=utf-8
+# ------------------------------------------------------------------------
+"""
+``rebuild_term2``
+---------------------
+
+``rebuild_term2`` rebuilds your mptt pointers. Only use in emergencies.
+"""
+
+from django.core.management.base import NoArgsCommand
+
+from edw.models.term import TermModel
+
+class Command(NoArgsCommand):
+    help = "Run this manually to rebuild your mptt pointers. Only use in emergencies."
+
+    def handle_noargs(self, **options):
+        #print "Rebuilding MPTT pointers for TermModel"
+        TermModel._tree_manager.rebuild2()
