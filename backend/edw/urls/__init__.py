c17e2da7cc2c574208aaf0a5db20636d797debd0
Alexander Mishurov <alexander.m.mishurov@gmail.com>
2017-08-01 18:46:12 +0300

    NR-87 Add a functionality for the button

--- backend/edw/urls/__init__.py
+++ backend/edw/urls/__init__.py
@@ -5,8 +5,14 @@ from django.conf.urls import include, url
 from . import rest_api
 from . import auth
 
+from ..views.term import RebuildTreeView
+
 
 urlpatterns = (
+    url(r'^rebuild_tree/',
+        RebuildTreeView.as_view(),
+        name='rebuild_tree'
+    ),
     url(r'^api/', include(rest_api)),
     url(r'^auth/', include(auth)),
 )
