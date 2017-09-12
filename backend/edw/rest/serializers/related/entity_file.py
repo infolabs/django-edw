47f06b3461ea59a84ec1d9e231df9965229907dc
ARTEM YAROVOY <yarovoy@excentrics.ru>
2017-09-12 11:41:47 +0300

    Image and File serializers bug fix

--- backend/edw/rest/serializers/related/entity_file.py
+++ backend/edw/rest/serializers/related/entity_file.py
@@ -6,31 +6,30 @@ from django.utils.translation import ugettext_lazy as _
 
 from rest_framework import serializers
 
-from edw.models.related import EntityImageModel
-
-from edw.rest.serializers.filer_fields import FilerImageField
+from edw.models.related import EntityFileModel
+from edw.rest.serializers.filer_fields import FilerFileField
 
 
 class EntityFileSerializer(serializers.ModelSerializer):
     """
     Common serializer for the EntityImage model.
     """
-    image = FilerImageField(max_length=None, use_url=True, upload_folder_name=_('Entities images'))
-    file = serializers.SerializerMethodField()
+    file = FilerFileField(max_length=None, use_url=True, upload_folder_name=_('Entities files'))
+    file_serial = serializers.SerializerMethodField()
+
     original_filename = serializers.SerializerMethodField()
     file_size = serializers.SerializerMethodField()
 
     class Meta:
-        model = EntityImageModel
+        model = EntityFileModel
         extra_kwargs = {'url': {'view_name': 'edw:{}-detail'.format(model._meta.model_name)}}
         fields = '__all__'
 
     def get_file_size(self, instance):
-        return instance.image._file_size
+        return instance.file._file_size
 
     def get_original_filename(self, instance):
-        return instance.image.original_filename
+        return instance.file.original_filename
 
     def get_file(self, instance):
-        return str(instance.image.file)
-
+        return str(instance.file.file)
--- backend/edw/rest/serializers/related/entity_image.py
+++ backend/edw/rest/serializers/related/entity_image.py
@@ -6,30 +6,29 @@ from django.utils.translation import ugettext_lazy as _
 
 from rest_framework import serializers
 
-from edw.models.related import EntityFileModel
-
-from edw.rest.serializers.filer_fields import FilerFileField
+from edw.models.related import EntityImageModel
+from edw.rest.serializers.filer_fields import FilerImageField
 
 
 class EntityImageSerializer(serializers.ModelSerializer):
     """
     Common serializer for the EntityImage model.
     """
-    file = FilerFileField(max_length=None, use_url=True, upload_folder_name=_('Entities files'))
-    file_serial = serializers.SerializerMethodField()
+    image = FilerImageField(max_length=None, use_url=True, upload_folder_name=_('Entities images'))
+    file = serializers.SerializerMethodField()
     original_filename = serializers.SerializerMethodField()
     file_size = serializers.SerializerMethodField()
 
     class Meta:
-        model = EntityFileModel
+        model = EntityImageModel
         extra_kwargs = {'url': {'view_name': 'edw:{}-detail'.format(model._meta.model_name)}}
         fields = '__all__'
 
     def get_file_size(self, instance):
-        return instance.file._file_size
+        return instance.image._file_size
 
     def get_original_filename(self, instance):
-        return instance.file.original_filename
+        return instance.image.original_filename
 
     def get_file(self, instance):
-        return str(instance.file.file)
+        return str(instance.image.file)
