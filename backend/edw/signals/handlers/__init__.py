6f3c6995590df3ca1f903032c2ba7bc71cb7d44b
Rebus <romanov@excentrics.ru>
2017-10-25 16:49:58 +0300

    Поправил импорт

--- backend/edw/signals/handlers/__init__.py
+++ backend/edw/signals/handlers/__init__.py
@@ -1,25 +1,22 @@
 # -*- coding: utf-8 -*-
-from django.db.utils import ProgrammingError
+from django.db.utils import ProgrammingError, ImproperlyConfigured
 
 try:
+
     from . import (
         term,
         data_mart,
         entity,
         auth,
         notification,
-        postal_zone
     )
+
+    try:
+        from . import postal_zone
+    except ImproperlyConfigured:
+        pass
+
 except (AttributeError, ProgrammingError) as e:
     # initial migrations hack
     print("*** INITIAL MIGRATIONS HACK ***")
     print e.args
-
-
-# from . import (
-#     term,
-#     data_mart,
-#     entity,
-#     auth,
-#     notification
-# )
