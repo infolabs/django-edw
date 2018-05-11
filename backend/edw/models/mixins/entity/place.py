eeca1da9aa7d1458190df419aea43f3d26974788
ARTEM YAROVOY <yarovoy@excentrics.ru>
2018-05-11 18:38:43 +0300

    Переработан механизм определения места, теперь если сервис геокодера не смог определить геопозицию система устанавливает термин terra-incognita, а в качестве наименование места указывается его координаты.

--- backend/edw/models/mixins/entity/place.py
+++ backend/edw/models/mixins/entity/place.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+
+from django.db.models import Q
 from django.utils.functional import cached_property
 from django.utils.translation import ugettext_lazy as _
 
 from edw.signals.place import zone_changed
-from edw.utils.geo import get_location_from_geocoder, get_postcode
+from edw.utils.geo import get_location_from_geocoder, get_postcode, GeocoderException
 
 from edw.models.postal_zone import get_all_postal_zone_terms_ids, get_postal_zone
 from edw.models.term import TermModel
@@ -17,6 +19,7 @@ class PlaceMixin(object):
     REQUIRED_FIELDS = ('geoposition',)
 
     REGION_ROOT_TERM_SLUG = "region"
+    TERRA_INCOGNITA_TERM_SLUG = "terra-incognita"
 
     @classmethod
     def validate_term_model(cls):
@@ -27,15 +30,52 @@ class PlaceMixin(object):
                 slug=cls.REGION_ROOT_TERM_SLUG,
                 parent=None,
                 name=_('Region'),
-                semantic_rule=TermModel.OR_RULE,
+                semantic_rule=TermModel.XOR_RULE,
                 system_flags=(TermModel.system_flags.delete_restriction |
                               TermModel.system_flags.change_parent_restriction |
                               TermModel.system_flags.change_slug_restriction))
             region.save()
-
-        print(">>>> validate_term_model", cls, region)
-
+        try: # terra-incognita
+            region.get_descendants(include_self=False).get(slug=cls.TERRA_INCOGNITA_TERM_SLUG)
+        except TermModel.DoesNotExist:
+            terra_incognita = TermModel(
+                slug=cls.TERRA_INCOGNITA_TERM_SLUG,
+                parent_id=region.id,
+                name=_('Terra Incognita'),
+                semantic_rule=TermModel.OR_RULE,
+                system_flags=(TermModel.system_flags.delete_restriction |
+                              TermModel.system_flags.change_parent_restriction |
+                              TermModel.system_flags.change_slug_restriction |
+                              TermModel.system_flags.has_child_restriction))
+            terra_incognita.save()
         super(PlaceMixin, cls).validate_term_model()
+
+    @staticmethod
+    def get_region_term():
+        region = getattr(EntityModel, "_region_term_cache", None)
+        if region is None:
+            try:
+                region = TermModel.objects.get(slug=PlaceMixin.REGION_ROOT_TERM_SLUG, parent=None)
+            except TermModel.DoesNotExist:
+                pass
+            else:
+                EntityModel._region_term_cache = region
+        return region
+
+    @staticmethod
+    def get_terra_incognita_term():
+        terra_incognita = getattr(EntityModel, "_terra_incognita_cache", None)
+        if terra_incognita is None:
+            region = PlaceMixin.get_region_term()
+            if region is not None:
+                try:
+                    terra_incognita = region.get_descendants(include_self=False).get(
+                        slug=PlaceMixin.TERRA_INCOGNITA_TERM_SLUG)
+                except TermModel.DoesNotExist:
+                    pass
+                else:
+                    EntityModel._terra_incognita_cache = terra_incognita
+        return terra_incognita
 
     def need_terms_validation_after_save(self, origin, **kwargs):
         if (origin is None or origin.geoposition != self.geoposition) and self.geoposition:
@@ -53,23 +93,33 @@ class PlaceMixin(object):
 
     def validate_terms(self, origin, **kwargs):
         context = kwargs["context"]
-
         if (context.get("force_validate_terms", False) and not context.get("bulk_force_validate_terms", False)
         ) or context.get("validate_place", False):
             # нельзя использовать в массовых операциях из ограничения API геокодера
+            terra_incognita = self.get_terra_incognita_term()
             if origin is not None:
                 postal_zone_terms_ids = get_all_postal_zone_terms_ids()
                 to_remove = EntityModel.terms.through.objects.filter(
-                    entity_id=self.id, term_id__in=postal_zone_terms_ids).values_list('term_id', flat=True)
+                    Q(entity_id=self.id) &
+                    (Q(term_id__in=postal_zone_terms_ids) | Q(term_id=terra_incognita.id))
+                ).values_list('term_id', flat=True)
                 self.terms.remove(*to_remove)
             else:
                 to_remove = []
-            zone = get_postal_zone(get_postcode(self.location))
-            if zone is not None:
-                to_add = [zone.term.id,]
-                self.terms.add(*to_add)
+
+            try:
+                postcode = get_postcode(self.location)
+            except GeocoderException:
+                zone = None
             else:
-                to_add = []
+                zone = get_postal_zone(postcode)
+
+            if zone is not None:
+                to_add = [zone.term.id]
+            else:
+                to_add = [terra_incognita.id]
+            self.terms.add(*to_add)
+
             if set(to_add) != set(to_remove):
                 zone_changed.send(sender=self.__class__, instance=self,
                                   zone_term_ids_to_remove=to_remove,
