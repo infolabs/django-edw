b5891919cc8d77532efebefb8a0d40fd6d08632f
Rebus <romanov@excentrics.ru>
2018-07-14 13:21:32 +0300

    Merge branch 'master' of https://bitbucket.org/info-labs/alder
    # Conflicts:
    #	alder/assets/css/styles.min.css

--- backend/edw/migrations/0001_initial.py
+++ backend/edw/migrations/0001_initial.py
@@ -10,17 +10,4 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('post_office', '0001_initial'),
-    ]
-
-    operations = [
-        migrations.CreateModel(
-            name='Email',
-            fields=[
-            ],
-            options={
-                'proxy': True,
-            },
-            bases=('post_office.email',),
-        ),
     ]
