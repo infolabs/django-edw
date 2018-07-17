4ed72725e3dac8ea8f46a0855efa0aa98bf1b3c6
Rebus <romanov@excentrics.ru>
2018-07-17 15:00:29 +0300

    поправил импорт в разрещениях

--- backend/edw/rest/permissions.py
+++ backend/edw/rest/permissions.py
@@ -1,31 +1,33 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-
 from rest_framework import permissions
 
-from filer.fields.file import FilerFileField
 
+try:
+    from filer.fields.file import FilerFileField
+except ImportError:
+    pass
+else:
+    class IsFilerFileOwnerOrReadOnly(permissions.BasePermission):
+        """
+        Object-level permission to only allow owners of an object to edit it.
+        Assumes the model instance has an `file.owner` attribute.
+        """
 
-class IsFilerFileOwnerOrReadOnly(permissions.BasePermission):
-    """
-    Object-level permission to only allow owners of an object to edit it.
-    Assumes the model instance has an `file.owner` attribute.
-    """
+        def has_object_permission(self, request, view, obj):
+            # Read permissions are allowed to any request,
+            # so we'll always allow GET, HEAD or OPTIONS requests.
+            if request.method in permissions.SAFE_METHODS:
+                return True
 
-    def has_object_permission(self, request, view, obj):
-        # Read permissions are allowed to any request,
-        # so we'll always allow GET, HEAD or OPTIONS requests.
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        # Instance must have an attribute: `FilerFileField` object.
-        for field in obj._meta.fields:
-            if isinstance(field, FilerFileField):
-                f = getattr(obj, field.name)
-                return f.owner == request.user
-        else:
-            return False
+            # Instance must have an attribute: `FilerFileField` object.
+            for field in obj._meta.fields:
+                if isinstance(field, FilerFileField):
+                    f = getattr(obj, field.name)
+                    return f.owner == request.user
+            else:
+                return False
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
