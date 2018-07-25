c74aad4d18cf51ec609c9564ffb531fddea2972e
Alexander Mishurov <alexander.m.mishurov@gmail.com>
2018-07-25 13:42:10 +0300

    NR-391 Read entity from HTML, singeltonjs

--- frontend/src/actions/EntityActions.js
+++ frontend/src/actions/EntityActions.js
@@ -1,3 +1,7 @@
+import Singleton from 'singleton-js-es6';
+
+const globalStore = new Singleton();
+
 import {
     LOAD_ENTITY_ITEM,
     LOAD_ENTITIES,
@@ -14,7 +18,6 @@ import reCache from '../utils/reCache';
 export function getEntityItem(data) {
   return (dispatch, getState) => {
     if ( !getState().entities.loadingItems[data.id] ) {
-
       dispatch(loadingEntityItem(data.id));
 
       fetch(reCache(data.entity_url), {
@@ -79,7 +82,20 @@ export function getEntities(mart_id, subj_ids=[], options_obj = {}, options_arr 
             json: json,
             request_options: options_obj
           }));
-    }
+    };
+}
+
+export function readEntities(mart_id, options_obj = {}) {
+    let json = globalStore.initial[mart_id];
+    // add cookies to data
+    json.results.meta = Object.assign(json.results.meta, options_obj);
+    return dispatch => {
+        dispatch({
+            type: LOAD_ENTITIES,
+            json: json,
+            request_options: options_obj
+        });
+    };
 }
 
 export function showDescription(entity_id = null) {
--- frontend/src/components/BaseEntities.js
+++ frontend/src/components/BaseEntities.js
@@ -74,8 +74,13 @@ class BaseEntities extends Component {
 
     this.props.actions.notifyLoadingEntities();
 
+    /*
     this.props.actions.getEntities(
       entry_point_id, subj_ids, request_options, options_arr
+    );
+    */
+    this.props.actions.readEntities(
+      entry_point_id, request_options
     );
   }
 
