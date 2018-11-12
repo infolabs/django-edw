5e997411360b713eb637123197223e006bba5254
ARTEM YAROVOY <yarovoy@excentrics.ru>
2018-11-12 12:22:30 +0300

    add updaye_images action

--- backend/edw/admin/entity/actions/__init__.py
+++ backend/edw/admin/entity/actions/__init__.py
@@ -1,9 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+
+from django.core.exceptions import ImproperlyConfigured
+
+try:
+    from edw.models.related.entity_image import EntityImageModel
+    EntityImageModel() # Test pass if model materialized
+except (ImproperlyConfigured, ImportError):
+    pass
+else:
+    from update_images import update_images
+
 from update_terms import update_terms
 from update_relations import update_relations
-# from update_images import update_images
 from update_additional_characteristics_or_marks import update_additional_characteristics_or_marks
 from update_related_data_marts import update_related_data_marts
 from update_states import update_states
