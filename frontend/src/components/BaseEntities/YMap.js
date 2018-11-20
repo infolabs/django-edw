e7775d266db484aede5bd18ddcd0ddb4bef3f3c4
Alexander Mishurov <alexander.m.mishurov@gmail.com>
2018-11-20 07:03:42 +0300

    NR-459 Remove link, fix zoom, add circle icon

--- frontend/src/components/BaseEntities/YMap.js
+++ frontend/src/components/BaseEntities/YMap.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
-import { YMaps, Map, Placemark } from 'react-yandex-maps';
+import { withYMaps, YMaps, Map, Placemark } from 'react-yandex-maps';
 
 import AbstractMap from 'components/BaseEntities/AbstractMap';
 import { MAP_HEIGHT } from 'constants/Components';
@@ -9,6 +9,7 @@ import { MAP_HEIGHT } from 'constants/Components';
 const defaultState = {
   center: [55.76, 37.64],
   zoom: 15,
+  margin: 50,
   type: 'yandex#map',
   controls: [
     'fullscreenControl',
@@ -26,15 +27,15 @@ const defaultState = {
 const mapModules = [
   'control.FullscreenControl',
   'control.GeolocationControl',
-  'control.ZoomControl',
+  'control.ZoomControl'
 ];
 
 const markerModules = [
-  'geoObject.addon.balloon'
+  'geoObject.addon.balloon',
 ];
 
 
-export default class YMap extends AbstractMap {
+class YMapInner extends AbstractMap {
   setMapRef = ref => { this._map = ref; };
 
   closeBalloon(e) {
@@ -45,6 +46,33 @@ export default class YMap extends AbstractMap {
     e1.get('target').balloon.events.add('click', e2 => {
       this.handleInfoMouseClick(e2, data);
     });
+  }
+
+  componentWillMount() {
+    const style = `width: {{ options.diameter }}px;
+                   height: {{ options.diameter }}px;
+                   line-height: {{ options.diameter }}px;
+                   position: relative;`;
+
+    const circle = `
+      <div style="${style}">
+        <svg xmlns="http://www.w3.org/2000/svg"
+             style="position: absolute;"
+             width="100%" height="100%"
+             viewBox="0 0 100 100">
+           <circle cx="50" cy="50" r="40"
+            stroke="#121212"
+            stroke-width="1.5"
+            fill="{{ options.color }}"
+            vector-effect="non-scaling-stroke"/>
+        </svg>
+        <div style="position: absolute; height: 100%; width: 100%; text-align: center;">
+            {{ properties.iconContent }}
+        </div>
+      </div>
+    `;
+
+    this.circleLayout = this.props.ymaps.templateLayoutFactory.createClass(circle);
   }
 
   render() {
@@ -70,62 +98,79 @@ export default class YMap extends AbstractMap {
             info = this.assembleInfo(item, meta),
             balloonContent = ReactDOMServer.renderToString(info);
 
-      const marker = {
+      let marker = {
         center: [lat, lng],
         properties: {
           balloonContent: balloonContent
         },
-        options: {
+        item: item
+      };
+
+      if (item.extra.group_size) {
+        const label = item.extra.group_size.toString();
+        marker.properties.iconContent = label;
+        marker.options = {
+          preset: {iconLayout: this.circleLayout},
+          iconColor: 'white',
+          iconDiameter: 17 + label.length * 12,
+          hideIconOnBalloonOpen: false
+        };
+      } else {
+        marker.options = {
           preset: 'islands#dotIcon',
           iconColor: '#' + pinColor,
           hideIconOnBalloonOpen: false
-        },
-        item: item
-      };
-      if (item.extra.group_size) {
-        marker.properties.iconContent = item.extra.group_size.toString();
-        marker.options.preset = 'islands#icon';
+        };
       }
+
       markers.push(marker);
     }
 
-    let center, zoom;
+    let mapState = defaultState;
 
     if ((!geoItems.length || !this.state.itemsChanged) && this._map) {
-      zoom = this._map.getZoom();
-      center = this._map.getCenter();
+      mapState.zoom = this._map.getZoom();
+      mapState.center = this._map.getCenter();
     } else {
-      zoom = this.calculateZoom(lngMin, lngMax, latMin, latMax);
-      const latMap = latMin + (latMax - latMin) / 2,
-            lngMap = lngMin + (lngMax - lngMin) / 2;
-      center = [latMap, lngMap];
+      delete mapState.zoom;
+      delete mapState.center;
+      mapState.bounds = [[latMin, lngMin],[latMax, lngMax]];
     }
 
-    if (!zoom && !geoItems.length)
-      return null;
+    return (
+      <div className={entitiesClass}>
+        <Map style={{ height: MAP_HEIGHT }}
+             state={mapState}
+             options={{suppressMapOpenBlock: true}}
+             modules={mapModules}
+             instanceRef={this.setMapRef}>
+          {markers.map(
+            (marker, i) => 
+            <Placemark key={i}
+                       modules={markerModules}
+                       geometry={marker.center}
+                       properties={marker.properties}
+                       options={marker.options}
+                       onGeometrychange={this.closeBalloon}
+                       onBalloonopen={e => this.handleMarkerClick(e, marker.item)}/>
+          )}
+        </Map>
+      </div>
+    );
+  }
+}
 
-    const mapState = Object.assign({}, defaultState, {zoom, center});
 
+const YMapWrapped = withYMaps(YMapInner, true, ['templateLayoutFactory']);
+
+
+export default class YMap extends React.Component {
+  render() {
     return (
       <YMaps>
-        <div className={entitiesClass}>
-          <Map style={{ height: MAP_HEIGHT }}
-               state={mapState}
-               modules={mapModules}
-               instanceRef={this.setMapRef}>
-            {markers.map(
-              (marker, i) => 
-              <Placemark key={i}
-                         modules={markerModules}
-                         geometry={marker.center}
-                         properties={marker.properties}
-                         options={marker.options}
-                         onGeometrychange={this.closeBalloon}
-                         onBalloonopen={e => this.handleMarkerClick(e, marker.item)}/>
-            )}
-          </Map>
-        </div>
+        <YMapWrapped {...this.props} />
       </YMaps>
     );
   }
 }
+
