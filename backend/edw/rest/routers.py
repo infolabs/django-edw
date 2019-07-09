fab4b3897478ef83a5de485cc63935a3f60240cc
Valera Litvinenko <rich-v@yandex.ru>
2019-07-09 15:36:24 +0300

    Документация

--- backend/edw/rest/routers.py
+++ backend/edw/rest/routers.py
@@ -17,8 +17,20 @@ __all__ = [
 
 
 class DefaultBulkRouter(BulkRouterMixin, DefaultRouter):
+    """
+    ENG: This router includes routes for the standard set of list, create, 
+    retrieve, update, partial_update and destroy actions, but additionally 
+    includes a default API root view, that returns a response containing hyperlinks 
+    to all the list views.
+    RUS: Маршрутизатор, содержащий маршруты для стандартного набора
+    и дополнительно включает в себя корневое представление API по умолчанию.
+    """
     pass
 
 
 class NestedSimpleBulkRouter(BulkRouterMixin, NestedSimpleRouter):
+    """
+     ENG: Router and relationship fields for working with nested resources
+     RUS: Маршрутизатор и поля отношений для работы с вложенными ресурсами
+     """
     pass
