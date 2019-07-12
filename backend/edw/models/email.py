0510c84dcbb27633a4493f89ab4d806424142b4d
Valera Litvinenko <rich-v@yandex.ru>
2019-07-12 18:10:30 +0300

    Документация

--- backend/edw/models/email.py
+++ backend/edw/models/email.py
@@ -16,11 +16,23 @@ from post_office.models import Email as OriginalEmail
 
 class Email(OriginalEmail):
     class Meta:
+        """
+        ENG: Proxy model is used to modify the behavior of the model.
+        RUS: Proxy-модель для изменения поведения модели.
+        """
         proxy = True
 
 
 class EmailManager(models.Manager):
+    """
+    ENG: Application EmailManager to manage emails sent by a Django application.
+    RUS: Приложение Менеджер почты. Управляет электронными письмами, отправленными приложением Django.
+    """
     def get_queryset(self):
+        """
+        ENG: Return QuerySet a list of email objects
+        RUS: Возвращает в запросе список имеющихся email.
+        """
         return Email.objects.get_queryset()
 
 OriginalEmail.add_to_class('objects', EmailManager())
