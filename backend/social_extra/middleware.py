1bcdf4aa2fd8ee622d86fcc82760efaa91359e24
Alexander Mishurov <alexander.m.mishurov@gmail.com>
2019-09-07 16:09:13 +0300

    NR-560 Fix AuthCancelled condition

--- backend/social_extra/middleware.py
+++ backend/social_extra/middleware.py
@@ -1,12 +1,12 @@
 from django.http import HttpResponseRedirect
 
-from social_core import exceptions as social_exceptions
+from social_core.exceptions import AuthCanceled
 from social_django.middleware import SocialAuthExceptionMiddleware
 
 
 class SocialAuthExceptionMiddleware(SocialAuthExceptionMiddleware):
     def process_exception(self, request, exception):
-        if hasattr(social_exceptions, 'AuthCanceled'):
+        if isinstance(exception, AuthCanceled) and hasattr(request, 'strategy'):
             return HttpResponseRedirect(request.strategy.session_get('next', '/'))
         else:
             raise exception
