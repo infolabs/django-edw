066ca8564e3d0c84a230f12538fa6e721f54cbfb
infolabs <kochura_pavel2000@mail.ru>
2020-04-30 09:26:09 +0300

    Сделал миксин для наследования компонента Tile

--- frontend/src/components/BaseEntities/Tile.js
+++ frontend/src/components/BaseEntities/Tile.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import marked from 'marked';
+import TileItemMixin from "./TileItemMixin";
 
 
 // Container
@@ -15,7 +14,7 @@ export default class Tile extends Component {
     return (
       <ul className={entities_class}>
         {items.map(
-          (child, i) => 
+          (child, i) =>
           <TileItem key={i} data={child} actions={actions} descriptions={descriptions} position={i} meta={meta}/>
         )}
       </ul>
@@ -25,98 +24,28 @@ export default class Tile extends Component {
 
 // Element
 
-class TileItem extends Component {
-
-  constructor() {
-    super();
-    this.state = {
-      h_pos: null,
-    };
-  }
-
-  handleMouseOver(e) {
-    this.toggleDescription(e);
-  }
-
-  handleMouseOut(e) {
-    this.toggleDescription(e);
-  }
-
-  handleMouseClick(e) {
-    const { data, actions, meta } = this.props;
-    if (data.extra && data.extra.group_size) {
-      actions.notifyLoadingEntities();
-      actions.expandGroup(data.id, meta);
-      e.preventDefault();
-      e.stopPropagation();
-    }
-  }
-
-  toggleDescription(e) {
-    const { data, actions, meta, descriptions } = this.props,
-          id = data.id;
-
-    if (this.getIsHover(e.clientX, e.clientY)) {
-      actions.showDescription(id);
-
-      if (data.extra && data.extra.group_size && !meta.alike && !descriptions.groups[id])
-        actions.getEntityItem(data, meta);
-
-      if ((data.extra && !data.extra.group_size) && !descriptions[id])
-        actions.getEntityItem(data);
-    } else {
-      actions.hideDescription(id);
-    }
-  }
-
-  getIsHover(clientX, clientY) {
-    const area = ReactDOM.findDOMNode(this),
-          areaRect = area.getBoundingClientRect(),
-          posX = clientX - areaRect.left,
-          posY = clientY - areaRect.top;
-
-    return posX >= 0 && posY >= 0 && posX <= areaRect.width && posY <= areaRect.height;
-  }
-
-  componentDidMount(x, y, z) {
-    const area = ReactDOM.findDOMNode(this),
-          info = area.getElementsByClassName("ex-description-wrapper")[0];
-          // areaRect = area.getBoundingClientRect(),
-    if (info) {
-      // HACK: для правильного определения размера в хроме функция вызывается с таймаутом
-      setTimeout(() => {
-        const infoRect = info.getBoundingClientRect(),
-              window_width = window.innerWidth,
-              width = 250, // todo: calculate width
-              left = infoRect.right,
-              h_pos = window_width < left + width ? "right" : "left";
-        this.setState({h_pos});
-      }, 10)
-    }
-  }
+class TileItem extends TileItemMixin {
 
   render() {
     const { data, position, meta, descriptions } = this.props,
-          url = data.extra && data.extra.url ? data.extra.url : data.entity_url,
           index = position + meta.offset,
-          group_size = data.extra && data.extra.group_size ? data.extra.group_size : 0;
+          groupSize = data.extra && data.extra.group_size ? data.extra.group_size : 0;
 
-    let group_digit;
-    if (group_size) {
-      group_digit = (
-        <span className="ex-digit">{group_size}</span>
+    let groupDigit;
+    if (groupSize) {
+      groupDigit = (
+        <span className="ex-digit">{groupSize}</span>
       );
     }
 
-    let li_class = "ex-catalog-item";
-    if (descriptions.opened[data.id]) {
-      li_class += " ex-state-description";
-    }
+    let liClass = "ex-catalog-item";
+    if (descriptions.opened[data.id])
+      liClass += " ex-state-description";
 
     let characteristics = data.short_characteristics || [],
         marks = data.short_marks || [];
 
-    let descriptions_data = group_size ? descriptions.groups : descriptions;
+    let descriptions_data = groupSize ? descriptions.groups : descriptions;
     // let related_data_marts = [];
     if (descriptions_data[data.id]) {
         characteristics = descriptions_data[data.id].characteristics || [];
@@ -124,107 +53,20 @@ class TileItem extends Component {
         // related_data_marts = descriptions[data.id].marks || [];
     }
 
-    let annotations = {};
-    if (data.extra) {
-      for (const [key, val] of Object.entries(data.extra)) {
-        if (val instanceof Object && 'name' in val && 'value' in val)
-          annotations[key] = val;
-      }
-    }
+    const descriptionBaloon = this.getDescriptionBaloon(data, characteristics),
+          title = groupSize && !meta.alike ? data.extra.group_name : data.entity_name,
+          itemContent = this.getItemContent(data, title, marks),
+          itemBlock = this.getItemBlock(descriptionBaloon, itemContent, groupDigit, groupSize);
 
-    let description_baloon;
-    if (characteristics.length) {
-      description_baloon = (
-        <div className="ex-description-wrapper">
-          <div className="ex-baloon">
-            <div className="ex-arrow"></div>
-            <ul className="ex-attrs">
-              {characteristics.map(
-                (child, i) =>
-                  <li  className="characteristic"
-                    data-path={child.path} key={i}
-                    data-view-class={child.view_class.join(" ")}>
-                    <strong>{child.name}:&nbsp;</strong>
-                    {child.values.join("; ")}
-                  </li>
-              )}
-              {Object.keys(annotations).map(
-                (key, i) =>
-                  <li className="annotation" key={i}
-                    data-view-class={key}>
-                    <strong>{annotations[key].name}:&nbsp;</strong>
-                    {annotations[key].value}
-                  </li>
-              )}
-            </ul>
-          </div>
-        </div>
-      );
-    }
-
-    const title = group_size && !meta.alike ? data.extra.group_name : data.entity_name;
-
-    const item_content = (
-      <div className="ex-wrap-action">
-        <div className="ex-media"
-             dangerouslySetInnerHTML={{__html: marked(data.media, {sanitize: false})}}>
-        </div>
-
-        <ul className="ex-ribbons">
-          {marks.map(
-            (child, i) =>
-              <li className="ex-wrap-ribbon"
-                  key={i}
-                  data-name={child.name}
-                  data-path={child.path}
-                  data-view-class={child.view_class.join(" ")}>
-                <div className="ex-ribbon">{child.values.join(", ")}</div>
-              </li>
-          )}
-        </ul>
-
-        <div className="ex-wrap-title">
-          <h4 className="ex-title">
-            <a href={url} title={title}>{title}</a>
-          </h4>
-        </div>
-      </div>
-    );
-
-    let item_block = (
-        <div className="ex-catalog-item-block"
-             onClickCapture={e => { ::this.handleMouseClick(e); } }>
-          {description_baloon}
-          {item_content}
-        </div>
-    );
-
-    if (group_size) {
-      item_block = (
-        <div className="ex-catalog-item-block ex-catalog-item-variants"
-             onClickCapture={e => { ::this.handleMouseClick(e); } }>
-          <div>
-            <div>
-              {description_baloon}
-              {group_digit}
-              {item_content}
-            </div>
-          </div>
-        </div>
-      );
-    }
-
-    let ret = (
-      <li className={li_class}
+    return(
+      <li className={liClass}
           data-horizontal-position={this.state.h_pos}
           data-vertical-position="center"
           data-index={index}
           onMouseOver={e => { ::this.handleMouseOver(e); } }
           onMouseOut={e => { ::this.handleMouseOut(e); } }>
-          {item_block}
+          {itemBlock}
       </li>
     );
-
-    return ret;
   }
 }
---
+++ frontend/src/components/BaseEntities/TileItemMixin.js
@@ -0,0 +1,168 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import marked from 'marked';
+
+
+export default class TileItemMixin extends Component{
+
+  constructor(){
+    super();
+    this.state = {
+      h_pos: null,
+    };
+  }
+
+  handleMouseOver(e){
+    this.toggleDescription(e);
+  }
+
+  handleMouseOut(e){
+    this.toggleDescription(e);
+  }
+
+  handleMouseClick(e){
+    const {data, actions, meta} = this.props;
+    if (data.extra && data.extra.group_size) {
+       actions.notifyLoadingEntities();
+       actions.expandGroup(data.id, meta);
+       e.preventDefault();
+       e.stopPropagation();
+    }
+  }
+
+  toggleDescription(e){
+    const {data, actions, meta, descriptions} = this.props,
+          id = data.id;
+
+    if (this.getIsHover(e.clientX, e.clientY)) {
+      actions.showDescription(id);
+
+      if (data.extra && data.extra.group_size && !meta.alike && !descriptions.groups[id])
+        actions.getEntityItem(data, meta);
+
+      if ((data.extra && !data.extra.group_size) && !descriptions[id])
+        actions.getEntityItem(data);
+
+    } else
+      actions.hideDescription(id);
+  }
+
+  getIsHover(clientX, clientY){
+    const area = ReactDOM.findDOMNode(this),
+          areaRect = area.getBoundingClientRect(),
+          posX = clientX - areaRect.left,
+          posY = clientY - areaRect.top;
+
+    return posX >= 0 && posY >= 0 && posX <= areaRect.width && posY <= areaRect.height;
+  }
+
+  componentDidMount(x, y, z){
+    const area = ReactDOM.findDOMNode(this),
+          info = area.getElementsByClassName("ex-description-wrapper")[0];
+    // areaRect = area.getBoundingClientRect(),
+    if (info) {
+      // HACK: для правильного определения размера в хроме функция вызывается с таймаутом
+      setTimeout(() => {
+        const infoRect = info.getBoundingClientRect(),
+              window_width = window.innerWidth,
+              width = 250, // todo: calculate width
+              left = infoRect.right,
+              h_pos = window_width < left + width ? "right" : "left";
+          this.setState({h_pos});
+      }, 10)
+    }
+  }
+
+  getDescriptionBaloon(data, characteristics){
+    let annotations = {};
+    if (data.extra) {
+      for (const [key, val] of Object.entries(data.extra)) {
+        if (val instanceof Object && 'name' in val && 'value' in val)
+          annotations[key] = val;
+      }
+    }
+
+    if (characteristics.length) {
+      return(
+        <div className="ex-description-wrapper">
+          <div className="ex-baloon">
+            <div className="ex-arrow"></div>
+            <ul className="ex-attrs">
+              {characteristics.map(
+                (child, i) =>
+                  <li  className="characteristic"
+                    data-path={child.path} key={i}
+                    data-view-class={child.view_class.join(" ")}>
+                    <strong>{child.name}:&nbsp;</strong>
+                    {child.values.join("; ")}
+                  </li>
+              )}
+              {Object.keys(annotations).map(
+                (key, i) =>
+                  <li className="annotation" key={i}
+                    data-view-class={key}>
+                    <strong>{annotations[key].name}:&nbsp;</strong>
+                    {annotations[key].value}
+                  </li>
+              )}
+            </ul>
+          </div>
+        </div>
+      );
+    }
+  }
+
+  getItemContent(data, title, marks){
+    const url = data.extra && data.extra.url ? data.extra.url : data.entity_url;
+    return(
+      <div className="ex-wrap-action">
+        <div className="ex-media"
+             dangerouslySetInnerHTML={{__html: marked(data.media, {sanitize: false})}}>
+        </div>
+
+        <ul className="ex-ribbons">
+          {marks.map(
+            (child, i) =>
+              <li className="ex-wrap-ribbon"
+                  key={i}
+                  data-name={child.name}
+                  data-path={child.path}
+                  data-view-class={child.view_class.join(" ")}>
+                <div className="ex-ribbon">{child.values.join(", ")}</div>
+              </li>
+          )}
+        </ul>
+
+        <div className="ex-wrap-title">
+          <h4 className="ex-title">
+            <a href={url} title={title}>{title}</a>
+          </h4>
+        </div>
+      </div>
+    );
+  }
+
+  getItemBlock(descriptionBaloon, itemContent, groupDigit, groupSize){
+    if (!groupSize){
+      return (
+        <div className="ex-catalog-item-block"
+             onClickCapture={e => { ::this.handleMouseClick(e); } }>
+          {descriptionBaloon}
+          {itemContent}
+        </div>
+      )
+    } else {
+      return (
+        <div className="ex-catalog-item-block ex-catalog-item-variants"
+             onClickCapture={e => { ::this.handleMouseClick(e); } }>
+          <div>
+            <div>
+              {descriptionBaloon}
+              {groupDigit}
+              {itemContent}
+            </div>
+          </div>
+        </div> )
+    }
+  }
+}
