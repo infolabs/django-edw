33d100f4bd39168a46941c64143df38fd4b1dad1
Rebus <romanov@excentrics.ru>
2020-05-26 13:14:18 +0300

    Поправил ошибку поиска

--- backend/edw/views/term.py
+++ backend/edw/views/term.py
@@ -9,7 +9,6 @@ from django.core import urlresolvers
 from rest_framework import filters
 from rest_framework.response import Response
 from rest_framework.generics import get_object_or_404
-from rest_framework import serializers
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAdminUser
 
