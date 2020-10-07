647d18b405c7fbcd0075975453ff4aaa595356e4
EagerBeager <yarovoy@excentrics.ru>
2020-10-07 17:49:07 +0300

    OSK-83
    Обавил агрегацию по статусам рассмотрения сообщений и 2 поля для сериализации тоторые возвращают Null если значение 0

--- backend/edw/rest/fields.py
+++ backend/edw/rest/fields.py
@@ -29,3 +29,27 @@ class FloatFieldNull2Zero(fields.FloatField):
         Возвращает числа с плавающей точкой, преобразованные в сериализуемые
         """
         return 0.0 if value is None else super(FloatFieldNull2Zero, self).to_representation(value)
+
+
+class IntegerFieldZero2Null(fields.IntegerField):
+    """
+    Класс поля с целочисленными данными
+    """
+
+    def to_representation(self, value):
+        """
+         Возвращает целое число если значение не равно 0, иначе Null
+        """
+        return None if value == 0 else super(IntegerFieldZero2Null, self).to_representation(value)
+
+
+class FloatFieldZero2Null(fields.FloatField):
+    """
+    Класс поля с целочисленными данными
+    """
+
+    def to_representation(self, value):
+        """
+         Возвращает целое число если значение не равно 0, иначе Null
+        """
+        return None if value == 0.0 else super(FloatFieldZero2Null, self).to_representation(value)
