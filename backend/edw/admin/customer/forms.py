18397c2e6e2d7ddebad4d83ffee16425e7ac4e9f
Roman Uskov <romanuskov94@gmail.com>
2020-10-17 08:59:45 +0300

    OSK-89. В админке добавил проверку на длину имени пользователя, изменил текст в поле подсказки, а также добавил изменения в файл с переводом.

--- backend/edw/admin/customer/forms.py
+++ backend/edw/admin/customer/forms.py
@@ -6,22 +6,37 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from django.utils.translation import ugettext_lazy as _
 
+USERNAME_FIELD_HELP_TEXT = _(
+    'Required field. Length must not exceed 30 characters. Only letters, digits and symbols @/./+/-/_ are accepted.'
+)
+USERNAME_LENGTH_VALIDATION_TEXT = _('Username length must not exceed 30 characters.')
 
-#==============================================================================
+
+# ==============================================================================
 # CustomerCreationForm
-#==============================================================================
+# ==============================================================================
 class CustomerCreationForm(UserCreationForm):
     class Meta(UserChangeForm.Meta):
         model = get_user_model()
+
+    def __init__(self, *args, **kwargs):
+        super(UserCreationForm, self).__init__(*args, **kwargs)
+        self.fields['username'].help_text = USERNAME_FIELD_HELP_TEXT
+
+    def clean(self):
+        if 'username' in self.cleaned_data:
+            username = self.cleaned_data['username']
+            if username and len(username) > 30:
+                raise forms.ValidationError(USERNAME_LENGTH_VALIDATION_TEXT)
 
     def save(self, commit=True):
         self.instance.is_staff = True
         return super(CustomerCreationForm, self).save(commit=False)
 
 
-#==============================================================================
+# ==============================================================================
 # CustomerChangeForm
-#==============================================================================
+# ==============================================================================
 class CustomerChangeForm(UserChangeForm):
     email = forms.EmailField(required=False)
 
@@ -33,6 +48,13 @@ class CustomerChangeForm(UserChangeForm):
         instance = kwargs.get('instance')
         initial['email'] = instance.email or ''
         super(CustomerChangeForm, self).__init__(initial=initial, *args, **kwargs)
+        self.fields['username'].help_text = USERNAME_FIELD_HELP_TEXT
+
+    def clean(self):
+        if 'username' in self.cleaned_data:
+            username = self.cleaned_data['username']
+            if username and len(username) > 30:
+                raise forms.ValidationError(USERNAME_LENGTH_VALIDATION_TEXT)
 
     def clean_email(self):
         email = self.cleaned_data.get('email').strip()
