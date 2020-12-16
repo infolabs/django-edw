7c1f25ca91006a39d916fdf83bda81821ab2ecb7
Roman Uskov <romanuskov94@gmail.com>
2020-12-16 14:59:32 +0300

    OSK-135. добавил диаграммы для оценок модератора и частного лица, поправил стили и добавил новый цвет для оценки "разъяснено"

--- frontend/src/components/Aggregation.js
+++ frontend/src/components/Aggregation.js
@@ -20,10 +20,13 @@ class Aggregation extends Component {
         ret = (
           <ul className="ex-aggregation-attrs">
             {Object.keys(aggr).map(
-              (k, i) => <li key={i} data-slug={k}>
-                <strong>{ aggregation[k].name }:</strong>&nbsp;
-                { aggregation[k].value }
-              </li>
+              (k, i) => {
+                return (k.startsWith('num_private_person_assessment') || k.startsWith('num_moderator_assessment') ?
+                    null : <li key={i} data-slug={k}>
+                      <strong>{aggregation[k].name}:</strong>&nbsp;
+                      {aggregation[k].value}
+                    </li>)
+              }
             )}
           </ul>
         )
