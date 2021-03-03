780477d605d619c7e436e2961f5a806bfbf7179d
beloslav <velislav1329@gmail.com>
2021-03-03 17:51:13 +0300

    fix

--- backend/edw/tasks/clear_django_sessions.py
+++ backend/edw/tasks/clear_django_sessions.py
@@ -3,9 +3,6 @@ from django.core.management import call_command
 from io import StringIO
 from celery import shared_task
 
-import logging
-logger = logging.getLogger('logfile')
-
 
 @shared_task(name='clear_django_sessions')
 def clear_django_sessions():
