0caf35a23be60123c82ad6cc4422d3e3695964bf
Alexander Mishurov <mishurov@MacBook-Pro.local>
2021-05-26 15:04:34 +0300

    Add useEntities hook and use it in ProblemTile

--- frontend/src/mobile/components/Entities/ParticularProblemTile.js
+++ frontend/src/mobile/components/Entities/ParticularProblemTile.js
@@ -1,11 +1,11 @@
-import React, {Component} from 'react'
-import {ScrollView, View, ImageBackground, StyleSheet} from 'react-native'
-import {Text, Card, Layout} from '@ui-kitten/components'
-import {Badge} from 'native-base'
-import Singleton from '../../utils/singleton'
+import React from 'react';
+import {ScrollView, View, ImageBackground, StyleSheet} from 'react-native';
+import {Text, Card, Layout} from '@ui-kitten/components';
+import {Badge} from 'native-base';
+import Singleton from '../../utils/singleton';
 import Spinner from 'react-native-loading-spinner-overlay';
-import {tileStyles} from "../../styles/entities";
-import EntityMixin from "../BaseEntities/EntityMixin";
+import {tileStyles} from '../../styles/entities';
+import useEntities from '../BaseEntities/useEntities';
 
 
 const stylesComponent = StyleSheet.create({
@@ -13,85 +13,89 @@ const stylesComponent = StyleSheet.create({
     position: 'absolute',
     bottom: 15,
     left: 15,
-    zIndex: 4
+    zIndex: 4,
   },
   badgeText: {
-    color: '#fff'
-  }
+    color: '#fff',
+  },
 });
+
 
 const styles = Object.assign(tileStyles, stylesComponent);
 
-export default class ParticularProblemTile extends EntityMixin(Component) {
-  render() {
-    const {items, loading} = this.props;
-    const instance = Singleton.getInstance();
 
-    return (
-      <ScrollView scrollEventThrottle={2000}
-                  onScroll={e => this.handleScroll(e)}>
-        {loading ?
-          <View style={styles.spinnerContainer}>
-            <Spinner visible={true}/>
-          </View>
-          : null
-        }
-        <Layout style={styles.layout}>
-          {items.map(
-            (child, i) => <ParticularProblemTileItem key={i} data={child} domain={instance.Domain}
-                                                     maxLengthDescriptionTile={this.maxLengthDescriptionTile}/>
-          )}
-        </Layout>
-      </ScrollView>
-    );
-  }
-}
+function ParticularProblemTile(props) {
 
-class ParticularProblemTileItem extends Component {
-  render() {
-    const {data, domain, maxLengthDescriptionTile} = this.props,
-      {short_marks} = data;
+  const [maxLengthDescriptionTile, handleScroll] = useEntities(props);
 
-    if (data.media.match(/.*<img.*?src=('|")(.*?)('|")/))
-      data.media = `${domain}/${data.media.match(/.*<img.*?src=('|")(.*?)('|")/)[2]}`;
+  const {items, loading} = props;
+  const instance = Singleton.getInstance();
 
-    let textState = null,
-      backgroundColorState = 'gray';
-
-    short_marks.map(mark => {
-      if (mark.name === "Состояние" || mark.name === "Системное состояние") {
-        textState = mark.values[0];
-        mark.view_class.map(item => {
-          if (item.startsWith('pin-color-'))
-            backgroundColorState = `#${item.replace('pin-color-', '')}`;
-        })
-      }
-    });
-
-    return (
-      <Card style={styles.cardContainer} onPress={() => console.log(data.id)}>
-        <View style={styles.cardImageContainer}>
-          <ImageBackground source={{uri: data.media}} style={styles.imageBackground}>
-            <Text style={styles.entityNameText}>
-              {data.entity_name.length > maxLengthDescriptionTile ?
-                `${data.entity_name.slice(0, maxLengthDescriptionTile)}...`
-                : data.entity_name
-              }
-            </Text>
-            {textState ?
-              <Badge style={{...styles.badge, backgroundColor: backgroundColorState}}>
-                <Text style={styles.badgeText}>
-                  {textState.length > 12 ?
-                    `${textState.slice(0, 12)}...`
-                    : textState
-                  }
-                </Text>
-              </Badge>
-              : null
-            }
-          </ImageBackground>
+  return (
+    <ScrollView scrollEventThrottle={2000} onScroll={handleScroll}>
+      {loading ?
+        <View style={styles.spinnerContainer}>
+          <Spinner visible={true}/>
         </View>
-      </Card>
-    )
-  }
+        : null
+      }
+      <Layout style={styles.layout}>
+        {items.map(
+          (child, i) => <ParticularProblemTileItem key={i} data={child} domain={instance.Domain}
+                          maxLengthDescriptionTile={maxLengthDescriptionTile}/>
+        )}
+      </Layout>
+    </ScrollView>
+  );
 }
+
+
+function ParticularProblemTileItem(props) {
+  const {data, domain, maxLengthDescriptionTile} = props,
+    {short_marks} = data;
+
+  if (data.media.match(/.*<img.*?src=('|")(.*?)('|")/))
+    data.media = `${domain}/${data.media.match(/.*<img.*?src=('|")(.*?)('|")/)[2]}`;
+
+  let textState = null,
+    backgroundColorState = 'gray';
+
+  short_marks.map(mark => {
+    if (mark.name === 'Состояние' || mark.name === 'Системное состояние') {
+      textState = mark.values[0];
+      mark.view_class.map(item => {
+        if (item.startsWith('pin-color-'))
+          backgroundColorState = `#${item.replace('pin-color-', '')}`;
+      });
+    }
+  });
+
+  return (
+    <Card style={styles.cardContainer} onPress={() => console.log(data.id)}>
+      <View style={styles.cardImageContainer}>
+        <ImageBackground source={{uri: data.media}} style={styles.imageBackground}>
+          <Text style={styles.entityNameText}>
+            {data.entity_name.length > maxLengthDescriptionTile ?
+              `${data.entity_name.slice(0, maxLengthDescriptionTile)}...`
+              : data.entity_name
+            }
+          </Text>
+          {textState ?
+            <Badge style={{...styles.badge, backgroundColor: backgroundColorState}}>
+              <Text style={styles.badgeText}>
+                {textState.length > 12 ?
+                  `${textState.slice(0, 12)}...`
+                  : textState
+                }
+              </Text>
+            </Badge>
+            : null
+          }
+        </ImageBackground>
+      </View>
+    </Card>
+  );
+}
+
+
+export default ParticularProblemTile;
