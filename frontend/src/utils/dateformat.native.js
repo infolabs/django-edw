18269a1a21d5f9b22d72842580b77c21045eb84f
Pavel1711 <45904265+Pavel1711@users.noreply.github.com>
2021-07-28 13:07:09 +0300

    Внедрил функционал комментариев

--- frontend/src/utils/dateformat.native.js
+++ frontend/src/utils/dateformat.native.js
@@ -1,9 +1,14 @@
-export const dateFormat = (dateFormatted) => {
-  const d = new Date(dateFormatted);
+export const dateFormat = (date, onlyDate = true) => {
+  const d = new Date(date);
   const day = d.getDate();
   const monthIndex = d.getMonth();
   const year = d.getFullYear();
-  return `${day} ${MONTHS[monthIndex]} ${year} г.`
+  if (onlyDate)
+    return `${day} ${MONTHS[monthIndex]} ${year} г.`;
+
+  const hour = d.getHours();
+  const minutes = d.getMinutes();
+  return `${day} ${MONTHS[monthIndex]} ${year} г. ${hour > 10 ? hour : `0${hour}`}:${minutes > 10 ? minutes : `0${minutes}`}`;
 };
 
-const MONTHS = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря']
+const MONTHS = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
