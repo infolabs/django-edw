2ec17bed9d2f65aeb671be7b4b25b910dd0c7dd5
Романов Сергей <romanov@infolabs.ru>
2022-04-20 13:01:03 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -61,8 +61,6 @@ setup(
     packages=find_packages(exclude=['docs', 'requirements']),
     package_dir={
         'edw': 'backend/edw',
-        'email_auth': 'backend/email_auth',
-        'social_extra': 'backend/social_extra',
     },
     include_package_data=True,
     zip_safe=False,
