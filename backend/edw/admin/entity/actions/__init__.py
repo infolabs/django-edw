e23ee8bc91491b8029495a1b25712e4b4bae281e
cawwot <worryd@mail.ru>
2024-07-19 21:23:52 +0300

    ALL-1861 update_active action removed

--- backend/edw/admin/entity/actions/__init__.py
+++ backend/edw/admin/entity/actions/__init__.py
@@ -17,7 +17,6 @@ from .update_relations import update_relations
 from .update_additional_characteristics_or_marks import update_additional_characteristics_or_marks
 from .update_related_data_marts import update_related_data_marts
 from .update_states import update_states
-# from .update_active import update_active
 from .force_validate import force_validate
 from .make_terms_by_additional_attrs import make_terms_by_additional_attrs
 from .normalize_additional_attrs import normalize_additional_attrs
