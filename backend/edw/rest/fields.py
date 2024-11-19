14dfe462624185e3ad71ba7c1bff74a0c831c428
eagerbeager <yarovoy@excentrics.ru>
2024-11-19 17:00:32 +0300

    Refactor note form and edw

--- backend/edw/rest/fields.py
+++ backend/edw/rest/fields.py
@@ -1,8 +1,39 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-
 from rest_framework import fields
+
+
+class ConvertNoneToStringSerializerMixin():
+    """
+    Mixin to convert None to empty strings.
+
+    This must be added as the first inherited class. The property `Meta.none_to_str_fields` must
+    be defined in order for this to have any effect. This only applies to representations,
+    when we export our instance data, not when we acquire and validate data.
+
+    This is how you would use it:
+    class AttendanceSerializer(ConvertNoneToStringSerializerMixin, serializers.ModelSerializer):
+        class Meta:
+            model = Attendance
+            fields = ('id', 'face_image')
+            none_to_str_fields = ('face_image', )
+    """
+    def get_none_to_str_fields(self):
+        meta = getattr(self, 'Meta', None)
+        return getattr(meta, 'none_to_str_fields', [])
+
+    def to_representation(self, instance):
+        fields = self.get_none_to_str_fields()
+        data = super().to_representation(instance)
+
+        if not fields or not isinstance(data, dict):
+            return data
+
+        for field in fields:
+            if field in data and data[field] is None:
+                data[field] = ''
+        return data
 
 
 # Number types...
