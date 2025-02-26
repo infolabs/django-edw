37eee71634a7ca747764bb22e2558f5a2a18446d
eagerbeager <yarovoy@excentrics.ru>
2025-02-26 17:21:07 +0300

    'customer' proxy field bug fix

--- backend/edw/admin/entity/forms/set_foreign_key.py
+++ backend/edw/admin/entity/forms/set_foreign_key.py
@@ -8,6 +8,9 @@ from django.utils.translation import ugettext_lazy as _
 from edw.admin.base_actions import BaseActionAdminForm
 from edw.admin.entity.widgets.dynamic_raw_id import ModelIdWidget, ModelRel
 
+from edw.admin.customer import CustomerProxy
+from edw.models.customer import BaseCustomer
+
 
 
 class SetForeignKeyInEntitiesActionAdminForm(BaseActionAdminForm):
@@ -20,8 +23,9 @@ class SetForeignKeyInEntitiesActionAdminForm(BaseActionAdminForm):
 
         for field in [x for x in self.opts.fields if (isinstance(x, ForeignKey) and x.editable and
                                                       x.get_internal_type() == 'ForeignKey')]:
-
+            model_rel = ModelRel(CustomerProxy, 'customer') if issubclass(field.related_model, BaseCustomer) else (
+                ModelRel(field.related_model, field.related_model._meta.pk.name))
             self.fields[field.name] = forms.ModelChoiceField(
                 queryset=field.related_model.objects.all(), required=False, to_field_name=field.related_model._meta.pk.name,
-                widget=ModelIdWidget(admin.site, ModelRel(field.related_model, field.related_model._meta.pk.name)),
+                widget=ModelIdWidget(admin.site, model_rel),
                 label=_(field.verbose_name))
